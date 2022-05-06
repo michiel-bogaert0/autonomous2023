ad4bca9845075e2567aa6c6247d2f3977a04c4c7
Lucas Van Dijck <lucas.pa.vd@gmail.com>
2022-05-06 22:47:34 +0200

    write initial node

---
+++ ROS/src/jetson_can/scripts/can_listener.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import rospy
+import can
+from std_msgs.msg import String
+
+
+def listen_on_can() -> None:
+    """Listens on the can0 interface of the Jetson and publishes the messages"""
+
+    pub = rospy.Publisher("diagnostics", String, queue_size=10)
+
+    # create a bus instance
+    bus = can.interface.Bus(
+        bustype="socketcan",
+        channel=rospy.get_param("~interface", "can0"),
+        bitrate=rospy.get_param("~baudrate", 1000000),
+    )
+
+    # iterate over received messages
+    # this keeps looping forever
+    for msg in bus:
+        pub.publish("/diagnostics", f"[{msg.arbitration_id}] {msg.data}")
+
+
+if __name__ == "__main__":
+    rospy.init_node("jetson_can")
+
+    try:
+        listen_on_can()
+    except rospy.ROSInterruptException:
+        pass
