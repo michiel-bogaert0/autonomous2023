9f279a4afc89983ff45bcc99026b590b108bff96
Lucas Van Dijck <lucas.pa.vd@gmail.com>
2022-07-25 20:50:09 +0200

    fixed setups

--- tools/keypoint_detector/setup.py
+++ tools/keypoint_detector/setup.py
@@ -6,15 +6,8 @@ setuptools.setup(
     version="0.0.1",
     author="Gilles Ballegeer",
     author_email="gilles.ballegeer@ugent.be",
-    description="",
-    long_description="",
-    long_description_content_type="text/markdown",
+    description="Keypoint detector",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires=">=3.8",
     install_requires=[
     ]
