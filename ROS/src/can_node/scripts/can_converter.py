474cb81758951e80f5cbb2393ce5c6d469cff723
Lowiek Van den Stockt <lowiek@fablabfactory.com>
2022-07-31 11:51:16 +0200

    Fixed some small issues in the can node

--- ROS/src/can_node/scripts/can_converter.py
+++ ROS/src/can_node/scripts/can_converter.py
@@ -8,16 +8,28 @@ import struct
 from std_msgs.msg import String
 from geometry_msgs.msg import TwistWithCovarianceStamped, TwistWithCovariance, Twist
 
-WHEEL_DIAMETER = 8 * 2.54 / 100 # in m
 
-odrive_db = cantools.database.load_file('../odrive.dbc')
+odrive_db = cantools.database.load_file("../odrive.dbc")
+
 
 class CanConverter:
     def __init__(self):
         rospy.init_node("jetson_can")
         self.can_pub = rospy.Publisher("/can_messages", String, queue_size=10)
-        self.vel_left = rospy.Publisher("/mechatronics/wheel_encoder/left", TwistWithCovarianceStamped, queue_size=10)
-        self.vel_right = rospy.Publisher("/mechatronics/wheel_encoder/right", TwistWithCovarianceStamped, queue_size=10)
+        self.vel_left = rospy.Publisher(
+            "/mechatronics/wheel_encoder/left",
+            TwistWithCovarianceStamped,
+            queue_size=10,
+        )
+        self.vel_right = rospy.Publisher(
+            "/mechatronics/wheel_encoder/right",
+            TwistWithCovarianceStamped,
+            queue_size=10,
+        )
+
+        self.wheel_diameter = rospy.get_param("~wheel_diameter", 8 * 2.54 / 100)  # in m
+        self.axis0_frame = rospy.get_param("~axis0/frame", "ugr/car_axis0")
+        self.axis1_frame = rospy.get_param("~axis1/frame", "ugr/car_axis1")
 
         # create a bus instance
         self.bus = can.interface.Bus(
@@ -50,25 +62,32 @@ class CanConverter:
 
                     # Encoder estimate
                     twist_msg = TwistWithCovarianceStamped()
-                    twist_msg.header.frame_id = 'lowiek_gaat_dit_weten'
+                    twist_msg.header.frame_id = (
+                        self.axis0_frame if axis_id == 1 else self.axis1_frame
+                    )
+                    twist_msg.header.stamp = rospy.Time.from_sec(msg.timestamp)
+
                     twist_msg.twist = TwistWithCovariance()
-                    twist_msg.twist.covariance = np.eye(6)
+
+                    # TODO Use actual covariance measurements (first we need data to estimate these)
+                    twist_msg.twist.covariance = np.diag([0.1, 0.1, 0, 0, 0, 0])
                     twist_msg.twist.twist = Twist()
-                    
-                    speed = can_msg['Vel_Estimate'] # in rev/s
-                    speed *= np.pi * WHEEL_DIAMETER
+
+                    speed = can_msg["Vel_Estimate"]  # in rev/s
+                    speed *= np.pi * self.wheel_diameter
                     twist_msg.twist.twist.linear.x = speed
 
                     if axis_id == 1:
                         self.vel_right(twist_msg)
                     elif axis_id == 2:
                         self.vel_left(twist_msg)
-                    
+
                     continue
-            
 
             # If the message was not recognised, just post it to the general topic
-            self.can_pub.publish(f"{msg.timestamp} - [{msg.arbitration_id}] {msg.data.hex()}")
+            self.can_pub.publish(
+                f"{msg.timestamp} - [{msg.arbitration_id}] {msg.data.hex()}"
+            )
 
             # Check for external shutdown
             if rospy.is_shutdown():
@@ -76,16 +95,17 @@ class CanConverter:
 
     def send_on_can(self, msg: can.Message) -> None:
         """Sends a message msg over the can bus
-        
+
         Args:
             msg: the message to send
-            
+
         """
         try:
             self.bus.send(msg)
             rospy.loginfo(f"Message sent on {self.bus.channel_info}")
         except can.CanError:
             rospy.logerr("Message NOT sent")
+
 
 if __name__ == "__main__":
     try:
