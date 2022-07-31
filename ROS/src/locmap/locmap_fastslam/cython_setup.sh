ca1d3c176cea76a97939385aa96e7d4bc12fe524
Lucas Van Dijck <lucas.pa.vd@gmail.com>
2022-07-31 16:44:49 +0200

    moving locmap pkg under a subfolder and change CICD

---
+++ ROS/src/locmap/locmap_fastslam/cython_setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ./../../../src/locmap/locmap_fastslam
+python3 ./setup.py build_ext --inplace  
--- ROS/src/locmap_fastslam/cython_setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ./../../src/locmap_fastslam
-python3 ./setup.py build_ext --inplace
