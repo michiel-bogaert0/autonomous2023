b18a257a4db5f4cc7332c9a89a672250929291f1
Lucas Van Dijck <lucas.pa.vd@gmail.com>
2022-08-04 22:44:52 +0200

    added odrive control

--- ROS/src/mechatronics/can_node/scripts/odrive.py
+++ ROS/src/mechatronics/can_node/scripts/odrive.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class OdriveConverter:
-    def __init__(self):
+    def __init__(self, bus = None):
         self.vel_right = rospy.Publisher(
             "/output/vel0",
             TwistWithCovarianceStamped,
@@ -23,6 +23,9 @@ class OdriveConverter:
         self.axis1_frame = rospy.get_param("~axis1/frame", "ugr/car_axis1")
 
         self.odrive_db = cantools.database.load_file(rospy.get_param("~odrive_dbc", "../odrive.dbc"))
+
+        if bus is not None:
+            self.bus = bus
 
     def handle_odrive_vel_msg(self, msg : can.Message, axis_id: int, cmd_id: int) -> None:
         """Publishes an ODrive velocity message to the correct ROS topic
@@ -53,3 +56,52 @@ class OdriveConverter:
             self.vel_right.publish(twist_msg)
         elif axis_id == 2:
             self.vel_left.publish(twist_msg)
+
+class OdriveController:
+    def __init__(self):
+        self.vel_right = rospy.Publisher(
+            "/output/vel0",
+            TwistWithCovarianceStamped,
+            queue_size=10,
+        )
+
+        self.odrive_db = cantools.database.load_file(rospy.get_param("~odrive_dbc", "../odrive.dbc"))
+
+        # Test code
+        for i in range(100):
+            self.publish_torque_command(i/100, 0)
+            self.publish_torque_command(i/100, 1)
+
+    def publish_torque_command(self, torque: float, axis: int) -> None:
+        """Publishes a drive command with a given torque to the ODrive
+        
+        Args:
+            torque: the requested torque
+            axis: 0 is right, 1 is left
+        """
+        if self.bus is None:
+            rospy.logerr("The ODrive package was not configured to send messages, please run it as a separate node.")
+            return
+        
+        torque_msg = self.odrive_db.get_message_by_name('Set_Input_Torque')
+        data = torque_msg.encode({'Input_Torque': torque})
+
+        can_id = axis << 5 | 0xE
+
+        self.bus.send(can.Message(arbitration_id=can_id, data=data))
+
+
+
+
+if __name__ == "__main__":
+    try:
+        # create a bus instance
+        bus = can.interface.Bus(
+            bustype="socketcan",
+            channel=rospy.get_param("~interface", "can0"),
+            bitrate=rospy.get_param("~baudrate", 250000),
+        )
+
+        odc = OdriveController(bus)
+    except rospy.ROSInterruptException:
+        pass
