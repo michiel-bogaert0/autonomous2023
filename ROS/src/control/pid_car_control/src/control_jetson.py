06f0f05c923b21ddd61350342f8d9da733a8d1ed
Jetson <jetson@ugentracing.be>
2022-08-24 18:58:46 +0200

    Fixed som issues

--- ROS/src/control/pid_car_control/src/control_jetson.py
+++ ROS/src/control/pid_car_control/src/control_jetson.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import rospy
 import cantools
 import can
@@ -6,9 +7,9 @@ from fs_msgs.msg import ControlCommand
 MAX_VELOCITY = 3  # in rev/s
 STEER_MAX_STEP = 1500  # This must correspond to the value on the RC Teensy
 
-CAN_NODE_ID 0xE0
-CAN_REBOOT_ID 0x1
-CAN_STEER_ID 0x3
+CAN_NODE_ID = 0xE0
+CAN_REBOOT_ID = 0x1
+CAN_STEER_ID = 0x3
 
 class JetsonController:
     def __init__(self, bus=None):
@@ -20,8 +21,10 @@ class JetsonController:
         )
 
         self.pub = rospy.Subscriber(
-            "/input/drive_command", ControlCommand, self.publish_odrive_command
+            "/input/drive_command", ControlCommand, self.publish_drive_command
         )
+
+        rospy.spin()
 
     def publish_drive_command(self, msg: ControlCommand) -> None:
         """Gets called on each ControlCommand
@@ -50,7 +53,7 @@ class JetsonController:
         id = CAN_NODE_ID << 5 | CAN_STEER_ID
 
         msg = can.Message(
-            arbitration_id=id, data=steering
+            arbitration_id=id, data=steering, signed=True
         )
 
         self.bus.send(msg)
@@ -67,15 +70,13 @@ class JetsonController:
                 "The ODrive package was not configured to send messages, please run it as a separate node."
             )
             return
-
         vel_msg = self.odrive_db.get_message_by_name("Set_Input_Vel")
-        data = vel_msg.encode({"Input_Torque_FF": 0})
-        data = vel_msg.encode({"Input_Vel": vel})
+        data = vel_msg.encode({"Input_Torque_FF": 0, "Input_Vel": vel})
 
-        can_id = axis << 5 | 0xE
+        can_id = axis << 5 | 0x00E
 
         self.bus.send(
-            can.Message(arbitration_id=can_id, data=data, is_extended_id=False)
+            can.Message(arbitration_id=can_id, data=data, is_extended_id=True)
         )
 
 
