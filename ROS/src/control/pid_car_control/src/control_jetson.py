3bd7d46551652176f01e66910b334d6b2343fdf5
Jetson <jetson@ugentracing.be>
2022-08-24 20:03:46 +0200

    Fixed bugs

--- ROS/src/control/pid_car_control/src/control_jetson.py
+++ ROS/src/control/pid_car_control/src/control_jetson.py
@@ -4,7 +4,7 @@ import cantools
 import can
 from fs_msgs.msg import ControlCommand
 
-MAX_VELOCITY = 3  # in rev/s
+MAX_VELOCITY = 5  # in rev/s
 STEER_MAX_STEP = 1500  # This must correspond to the value on the RC Teensy
 
 CAN_NODE_ID = 0xE0
@@ -35,9 +35,9 @@ class JetsonController:
             return
 
         self.set_odrive_velocity(msg.throttle * MAX_VELOCITY, 1)
-        self.set_odrive_velocity(msg.throttle * MAX_VELOCITY, 2)
+        self.set_odrive_velocity(-1 * msg.throttle * MAX_VELOCITY, 2)
 
-        self.set_steering_setpoint(self, steering)
+        self.set_steering_setpoint(msg.steering)
         
     def set_steering_setpoint(self, steering: float) -> None:
         """Sends a CAN message to actuate the steering system
@@ -53,7 +53,7 @@ class JetsonController:
         id = CAN_NODE_ID << 5 | CAN_STEER_ID
 
         msg = can.Message(
-            arbitration_id=id, data=steering, signed=True
+            arbitration_id=id, data=steering.to_bytes(2, "little", signed=True)
         )
 
         self.bus.send(msg)
@@ -73,10 +73,10 @@ class JetsonController:
         vel_msg = self.odrive_db.get_message_by_name("Set_Input_Vel")
         data = vel_msg.encode({"Input_Torque_FF": 0, "Input_Vel": vel})
 
-        can_id = axis << 5 | 0x00E
+        can_id = axis << 5 | 0x00D
 
         self.bus.send(
-            can.Message(arbitration_id=can_id, data=data, is_extended_id=True)
+            can.Message(arbitration_id=can_id, data=data, is_extended_id=False)
         )
 
 
