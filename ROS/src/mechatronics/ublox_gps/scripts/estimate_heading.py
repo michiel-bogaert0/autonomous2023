b7eedbda5c459002a8d7aa3982ebd6af6cdc6f11
LowiekVDS <lowiek@fablabfactory.com>
2022-09-09 15:23:58 +0200

    Update estimate_heading.py

--- ROS/src/mechatronics/ublox_gps/scripts/estimate_heading.py
+++ ROS/src/mechatronics/ublox_gps/scripts/estimate_heading.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env python3
 
+from collections import deque
 from functools import partial
-from math import atan2, cos, pi, sin
-from typing import List
+from math import atan2, cos, pi, sin, sqrt
 
 import rospy
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point
 from node_fixture.node_fixture import ROSNode
 from sensor_msgs.msg import NavSatFix, Imu
 from tf.transformations import quaternion_from_euler
 
+class StreamingMovingAverage:
+    """
+    Class that averages streamed data (moving average)
+    """
+    def __init__(self, window_size):
+        """
+        Args:
+            window_size: the size of the window to use
+        """
+        self.window_size = window_size
+        self.values = []
+        self.sum = 0
+
+    def process(self, value):
+        """
+        Processes a new sample
+
+        Args:
+            value: the value to process
+
+        Returns:
+            the averaged result
+        """
+        self.values.append(value)
+        self.sum += value
+
+        if len(self.values) > self.window_size:
+            self.sum -= self.values.pop(0)
+        
+        return float(self.sum) / len(self.values)
 
 class HeadingEstimation(ROSNode):
     def __init__(self):
@@ -23,12 +53,22 @@ class HeadingEstimation(ROSNode):
         """
 
         super().__init__("gps_heading_estimation", False)
-    
-        self.max_time_deviation = rospy.get_param("~max_time_deviation", 0.1)
+
+        self.max_time_deviation = rospy.get_param("~max_time_deviation", 0.05)
         self.rate = rospy.get_param("~rate", 10)
         self.base_link_frame = rospy.get_param("~base_link_frame", "ugr/car_base_link")
+        self.averaging_window = rospy.get_param("~averaging_window", 4)
+        self.time_offset = rospy.get_param("~time_offset", 0.1)
+        self.max_distance = rospy.get_param("~max_distance", 2)
+        self.min_distance = rospy.get_param("~min_distance", 0.05)
 
-        self.gps_msgs: List[NavSatFix, NavSatFix] = [None, None]
+        self.gps_msgs = [deque([], 2) for i in range(2)]
+        self.gps_vel = [None, None]
+
+        self.offset = [0, 0]
+        self.heading_yaw = [0, 0]
+
+        self.yaw_averager = StreamingMovingAverage(self.averaging_window)
 
         # Add subscribers for both GPS's
         rospy.Subscriber(
@@ -42,41 +82,69 @@ class HeadingEstimation(ROSNode):
             partial(self.handle_gps, gpsIndex=1),
         )
 
-        # Publish the heading periodically, limited by self.rate
-        self.rosrate = rospy.Rate(self.rate, True)
-        while not rospy.is_shutdown():
-            self.publish_heading()
-            self.rosrate.sleep()
+        rospy.spin()
 
-    def publish_heading(self):
+    def publish_heading(self, gpsIndex):
         """
         Actually publishes the heading (if heading can be calculated)
         """
 
-        # No message? No heading!
-        if not self.gps_msgs[0] or not self.gps_msgs[1]:
+        if len(self.gps_msgs[gpsIndex]) < 2:
             return
 
-        # Time deviates to much? No heading!
-        if self.gps_msgs[0].header.stamp.to_sec() - self.gps_msgs[1].header.stamp.to_sec() > self.max_time_deviation:
+        # Take one
+        msg0 = self.gps_msgs[gpsIndex][1]       
+        msg1 = self.gps_msgs[gpsIndex].popleft()
+
+        current_time_sec = rospy.Time.now().to_sec()
+
+        # Actually calculate heading and distance
+        long0 = msg0.longitude * 2 * pi / 360
+        long1 = msg1.longitude * 2 * pi / 360
+        lat0 = msg0.latitude * 2 * pi / 360
+        lat1 = msg1.latitude * 2 * pi / 360
+
+        dlong = long1 - long0
+        dlat = lat1 - lat0
+
+        # Distance
+        R = 6371000
+        a = sin(dlat / 2) ** 2 + cos(lat0) * cos(lat1) * sin(dlong/2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1-a))
+
+        distance = R * c
+
+        bearing = atan2(sin(dlong) * cos(lat1), cos(lat0) * sin(lat1) - sin(lat0) * cos(lat1) * cos(dlong)) * (-1) - pi/2
+
+        if distance > self.max_distance:
+            rospy.logwarn(f"Distance between points of GPS {gpsIndex} is too long (d = {distance} m)")
             return
 
-        # Actually calculate heading
-        long0 = self.gps_msgs[0].longitude * 2 * pi / 360
-        long1 = self.gps_msgs[1].longitude * 2 * pi / 360
-        lat0 = self.gps_msgs[0].latitude * 2 * pi / 360
-        lat1 = self.gps_msgs[1].latitude * 2 * pi / 360
+        if distance < self.min_distance:
+            rospy.logwarn(f"Distance between points of GPS {gpsIndex} is too short (d = {distance} m)")
+            return
 
-        y = sin(long1 - long0) * cos(lat1)
-        x = cos(lat0) * sin(lat1) - sin(lat0) * cos(lat1) * cos(long1 - long0)
+        if abs(bearing - self.offset[gpsIndex] - self.heading_yaw[gpsIndex]) > pi and self.heading_yaw[gpsIndex] != 0:
+            self.offset[gpsIndex] += 2 * pi * (1 if bearing - self.heading_yaw[gpsIndex] - self.offset[gpsIndex] > 0 else -1 )
 
-        bearing = atan2(y, x) * (-1)
+        bearing -= self.offset[gpsIndex]
+
+        self.heading_yaw[gpsIndex] = bearing
+
+        bearing = self.yaw_averager.process(bearing)
+
+        bm = Point(z=bearing)
+
+        self.publish("/output/yaw", bm)
 
         # Publish as Imu message
         # TODO estimate covariance based on GPS fixes
+
         msg = Imu()
         msg.header.frame_id = self.base_link_frame
-        msg.header.stamp = rospy.Time.from_sec((self.gps_msgs[0].header.stamp.to_sec() + self.gps_msgs[1].header.stamp.to_sec()) / 2)
+        msg.header.stamp = rospy.Time.from_sec(      
+            current_time_sec - self.time_offset
+        )
 
         x, y, z, w = quaternion_from_euler(0, 0, bearing)
         msg.orientation.x = x
@@ -86,8 +154,6 @@ class HeadingEstimation(ROSNode):
 
         self.publish("/output/heading", msg)
 
-        self.gps_msgs = [None, None]
-
     def handle_gps(self, navsatfixMsg: NavSatFix, gpsIndex: int):
         """
         Handles an incoming NavSatFix message
@@ -95,8 +161,9 @@ class HeadingEstimation(ROSNode):
         Args:
             navsatfixMsg: the incoming NavSatFix message
             gpsIndex: the index of the gps (0 or 1)
-        """ 
-        self.gps_msgs[gpsIndex] = navsatfixMsg
+        """
+        self.gps_msgs[gpsIndex].append(navsatfixMsg)
+        self.publish_heading(gpsIndex)
 
 if __name__ == "__main__":
     try:
