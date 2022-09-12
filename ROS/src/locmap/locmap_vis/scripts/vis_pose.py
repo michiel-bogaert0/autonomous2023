c1cf35cda90f7f793069e69cbb807af547568ae6
Lowiek <lowiek@fablabfactory.com>
2022-09-12 13:51:09 +0200

    Updated locmap visualsiation

---
+++ ROS/src/locmap/locmap_vis/scripts/vis_pose.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import rospy
+from locmap_vis import LocMapVis
+from node_fixture.node_fixture import AddSubscriber, ROSNode
+from ugr_msgs.msg import Observations
+
+
+class VisPose(ROSNode):
+    """
+    This node connects locmap to other nodes when a simple remap is not enough.
+    """
+
+    def __init__(self):
+
+        super().__init__(f"locmap_vis_car_{rospy.Time.now().to_sec()}")
+        
+        self.blue_cone_model_url = rospy.get_param(
+            "~cone_models/blue",
+            "https://storage.googleapis.com/learnmakeshare_cdn_public/blue_cone_final.dae",
+        )
+        self.yellow_cone_model_url = rospy.get_param(
+            "~cone_models/yellow",
+            "https://storage.googleapis.com/learnmakeshare_cdn_public/yellow_cone_final.dae",
+        )
+
+        self.use_cones = rospy.get_param("~use_cones", True)        
+
+        self.vis_namespace = rospy.get_param("~namespace", "locmap_vis")
+        self.vis_lifetime = rospy.get_param("~lifetime", 3)
+        
+        self.vis_sample_color = rospy.get_param("~sample_color", "g")
+        self.vis_handler = LocMapVis(
+            [self.blue_cone_model_url, self.yellow_cone_model_url]
+        )
+
+    @AddSubscriber("/input/vis")
+    def handleOdom(self, msg: Observations):
+    
+        marker_array = self.vis_handler.observations_to_markerarray(
+            msg, self.vis_namespace + "/observations", 0, False, self.use_cones
+        )
+        
+        self.publish("/output/vis", marker_array)
+
+node = VisPose()
+node.start()
