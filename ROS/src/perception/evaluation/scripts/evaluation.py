d4caafcd250178342fa2b47bf575fd761317a826
tristan279 <tristandhoedt@gmail.com>
2022-09-19 13:48:32 +0200

    changes

--- ROS/src/perception/evaluation/scripts/evaluation.py
+++ ROS/src/perception/evaluation/scripts/evaluation.py
@@ -5,20 +5,21 @@ from sensor_msgs.msg import NavSatFix
 from ugr_msgs.msg import ConeLocation
 from collections import deque
 
+
 class ConeLogger:
     """
     class that logs position of cones
     """
 
     def __init__(self):
-        rospy.init_node("conelogger")
+        rospy.init_node("cone_logger")
         self.type = "n"
 
         # keep track of a sequence of gps messages
-        self.cone_position = deque([],maxlen=30)
+        self.cone_position = deque([], maxlen=30)
 
         # only the message with the least covariance will be published
-        self.covariances = deque([],maxlen=30)
+        self.covariances = deque([], maxlen=30)
 
         # use different gps's for blue and yellow cones
         self.subscriber_left = rospy.Subscriber(
@@ -30,6 +31,8 @@ class ConeLogger:
 
         self.cone_pusher = rospy.Publisher("/output/cones", ConeLocation, queue_size=10)
 
+        self.cone_colors = {"b": 0, "y": 1, "or": 2, "ol": 2}
+
     def handle_input(self) -> None:
         """
         Main function
@@ -37,8 +40,8 @@ class ConeLogger:
         while not rospy.is_shutdown():
             # take input from user (y = yellow cone, b = blue cone, ol = orange cone left, or = orange cone right )
             cone_type = input("insert type: ")
-            if cone_type not in ["y","b","or","ol"]:
-                rospy.loginfo("bad input type")
+            if cone_type not in self.cone_colors.keys():
+                rospy.logwarn("bad input type")
                 continue
             self.type = cone_type
             # sleep for three seconds -> the node will fill the cone_position and covariances list during that period
@@ -55,18 +58,17 @@ class ConeLogger:
         """
         Fills the list with gps messages coming from the right gps
         args:
-            gps_msg: message comming from the right ublox gps
+            gps_msg: message coming from the right ublox gps
         """
         if self.type == "y" or self.type == "ol":
             self.cone_position.append(gps_msg)
             self.covariances.append(sum(gps_msg.position_covariance))
 
-
     def record_blue_cone(self, gps_msg: NavSatFix) -> None:
         """
         Fills the list with gps messages coming from the left gps
         args:
-            gps_msg: message comming from the left ublox gps
+            gps_msg: message coming from the left ublox gps
         """
         if self.type == "b" or self.type == "or":
             self.cone_position.append(gps_msg)
@@ -79,12 +81,7 @@ class ConeLogger:
             cone_type: color of the cone (options are b(lue), y(ellow), or (orange right) or ol (orange left))
         """
         # get the cone location with the least covariance
-        if cone_type == "y":
-            cone_type_int = 1
-        elif cone_type == "b":
-            cone_type_int = 0
-        elif "o" in cone_type:
-            cone_type_int = 2
+        cone_type_int = self.cone_colors[cone_type]
 
         rospy.loginfo(len(self.cone_position))
         most_accurate = self.covariances.index(min(self.covariances))
