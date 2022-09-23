48ae33173026d2c806d17bcc680c6117767d907a
lowie314 <lowie.meganck@gmail.com>
2022-09-23 11:35:34 +0200

    refactor coneclustering/coneclassification

--- ROS/src/perception/lidar/include/cone_clustering.hpp
+++ ROS/src/perception/lidar/include/cone_clustering.hpp
@@ -1,6 +1,7 @@
 #ifndef CONECLUSTERING_HPP
 #define CONECLUSTERING_HPP
 
+#include "cone_classification.hpp"
 #include "geometry_msgs/Point32.h"
 #include "sensor_msgs/PointCloud.h"
 #include <pcl/common/common.h>
@@ -10,21 +11,6 @@
 #include <ros/ros.h>
 
 namespace ns_lidar {
-typedef struct {
-  geometry_msgs::Point32 pos;
-  bool is_cone;
-  float color;
-  double bounds[3];
-} ConeCheck;
-
-typedef struct{
-  double x;
-  double y;
-  double floor;
-  double height_cone = 0.325;
-  double half_width_cone = 0.114;
-} ConeModel;
-
 struct {
   bool operator()(pcl::PointXYZINormal a, pcl::PointXYZINormal b) const {
     return a.y < b.y;
@@ -43,15 +29,12 @@ public:
 private:
   ros::NodeHandle &n_;
 
+  ConeClassification coneClassification_;
+
   std::string clustering_method_; // Default: euclidian, others: string
   double cluster_tolerance_;      // The cone clustering tolerance (m)
-  double point_count_threshold_;   // How much % can the cone point count
-                                  // prediction be off from the actual count
   double min_distance_factor_; // distance around the cone that contains no
                             // other cones as a factor to the width of the cone
-  int minimal_points_cone_;
-  float minimal_height_cone_; //minimal height of cone above the floor threshold
-  double cone_shape_factor_; //how similar should a pointcloud be to the cone model
 
   sensor_msgs::PointCloud
   euclidianClustering(const pcl::PointCloud<pcl::PointXYZINormal>::Ptr &cloud,
@@ -60,9 +43,6 @@ private:
   stringClustering(const pcl::PointCloud<pcl::PointXYZINormal>::Ptr &cloud);
   sensor_msgs::PointCloud
       construct_message(std::vector<pcl::PointCloud<pcl::PointXYZINormal>>);
-  ConeCheck isCloudCone(pcl::PointCloud<pcl::PointXYZINormal> cone);
-  bool checkShape(pcl::PointCloud<pcl::PointXYZINormal> cone, Eigen::Vector4f centroid, bool orange);
-  float hypot3d(float a, float b, float c);
   float arctan(float x, float y);
 };
 } // namespace ns_lidar
--- ROS/src/perception/lidar/src/cone_clustering.cpp
+++ ROS/src/perception/lidar/src/cone_clustering.cpp
@@ -8,15 +8,15 @@
 
 namespace ns_lidar {
 // Constructor
-ConeClustering::ConeClustering(ros::NodeHandle &n) : n_(n) {
+ConeClustering::ConeClustering(ros::NodeHandle &n) : n_(n), coneClassification_(n) {
   // Get parameters
   n.param<std::string>("clustering_method", clustering_method_, "string");
   n.param<double>("cluster_tolerance", cluster_tolerance_, 0.4);
-  n.param<double>("point_count_threshold", point_count_threshold_, 0.5);
+  // n.param<double>("point_count_threshold", point_count_threshold_, 0.5);
   n.param<double>("min_distance_factor", min_distance_factor_, 1.5);
-  n.param<int>("minimal_points_cone", minimal_points_cone_, 0);
-  n.param<float>("minimal_height_cone", minimal_height_cone_, 0.05);
-  n.param<double>("cone_shape_factor", cone_shape_factor_, 0.3);
+  // n.param<int>("minimal_points_cone", minimal_points_cone_, 0);
+  // n.param<float>("minimal_height_cone", minimal_height_cone_, 0.05);
+  // n.param<double>("cone_shape_factor", cone_shape_factor_, 0.3);
 }
 
 /**
@@ -153,9 +153,9 @@ sensor_msgs::PointCloud ConeClustering::stringClustering(
       pcl::PointXYZINormal rightmost = cluster_rightmost[cluster_id];
 
       // This distance should be calculated using max(delta_azi, delta_r)
-      float r_point = ConeClustering::hypot3d(point.x, point.y, point.z);
+      float r_point = hypot3d(point.x, point.y, point.z);
       float r_rightmost =
-          ConeClustering::hypot3d(rightmost.x, rightmost.y, rightmost.z);
+          hypot3d(rightmost.x, rightmost.y, rightmost.z);
       float delta_r = std::abs(r_point - r_rightmost);
       float delta_arc = std::abs(std::atan2(point.x, point.y) -
                                  atan2(rightmost.x, rightmost.y)) *
@@ -260,7 +260,7 @@ sensor_msgs::PointCloud ConeClustering::construct_message(
 
   // iterate over each cluster
   for (pcl::PointCloud<pcl::PointXYZINormal> cluster : clusters) {
-    ConeCheck cone_check = ConeClustering::isCloudCone(cluster);
+    ConeCheck cone_check = coneClassification_.classify_cone(cluster);
 
     // only add clusters that are likely to be cones
     if (cone_check.is_cone) {
@@ -280,134 +280,4 @@ sensor_msgs::PointCloud ConeClustering::construct_message(
   return cluster_msg;
 }
 
-/**
- * @brief Checks whether a PC represents a cone and which color it has.
- *
- * @returns ConeCheck containing the cones coordinates and whether it is a cone
- */
-ConeCheck ConeClustering::isCloudCone(pcl::PointCloud<pcl::PointXYZINormal> cone) {
-  ConeCheck cone_check;
-
-  Eigen::Vector4f centroid;
-  Eigen::Vector4f min;
-  Eigen::Vector4f max;
-  pcl::compute3DCentroid(cone, centroid);
-  pcl::getMinMax3D(cone, min, max);
-
-  float bound_x = std::fabs(max[0] - min[0]);
-  float bound_y = std::fabs(max[1] - min[1]);
-  float bound_z = std::fabs(max[2] - min[2]);
-
-
-  // filter based on the shape of cones
-  if (cone.points.size() >= minimal_points_cone_ && centroid[2] - min[2] > minimal_height_cone_)
-  {
-    float dist = ConeClustering::hypot3d(centroid[0], centroid[1], centroid[2]);;
-    float num_points = 0.0;
-    bool is_orange = false;
-    if(bound_x < 0.3 && bound_y < 0.3 && bound_z < 0.4)
-    {
-      // Calculate the expected number of points that hit the cone
-      // Using the AMZ formula with w_c = average of x and y widths and r_v=0.35°
-      // and r_h=2048 points per rotation
-      num_points = (1 / 2.0f) * (0.325 / (2.0f * dist * VERT_RES_TAN)) *
-                        (0.228 / (2.0f * dist * HOR_RES_TAN));
-    }
-    else if(bound_x < 0.3 && bound_y < 0.3 && bound_z < 0.55){
-      // Calculate the expected number of points that hit the cone
-      // Using the AMZ formula with w_c = average of x and y widths and r_v=0.35°
-      // and r_h=2048 points per rotation
-      num_points = (1 / 2.0f) * (0.505 / (2.0f * dist * VERT_RES_TAN)) *
-                        (0.285 / (2.0f * dist * HOR_RES_TAN));
-      is_orange = true;
-    }
-
-    // We allow for some play in the point count prediction
-    if (dist != 0.0 && (std::abs(num_points - cone.points.size()) / num_points)  <
-        point_count_threshold_ && ConeClustering::checkShape(cone, centroid, is_orange)) {
-      cone_check.pos.x = centroid[0];
-      cone_check.pos.y = centroid[1];
-      cone_check.pos.z = centroid[2];
-      cone_check.bounds[0] = bound_x;
-      cone_check.bounds[1] = bound_y;
-      cone_check.bounds[2] = bound_z;
-      cone_check.is_cone = true;
-
-      // Eigen::Matrix<double, 3, 3> covariance_matrix;
-      // pcl::computeCovarianceMatrix(cone, covariance_matrix);
-
-      if(is_orange){
-        cone_check.color = 2;
-        return cone_check;
-      }
-      // ROS_INFO("variance x: %lf", covariance_matrix(1,1));
-      // calculate the convexity of the intensity to distinguish between blue
-      // and yellow;
-      Eigen::MatrixXd X_mat(cone.points.size(), 3);
-      Eigen::VectorXd Y_mat(cone.points.size(), 1);
-
-      // setup matrix
-      for (int i = 0; i < cone.points.size(); i++) {
-        float z_value = cone.points[i].z;
-        X_mat(i, 0) = z_value * z_value;
-        X_mat(i, 1) = z_value;
-        X_mat(i, 2) = 1;
-
-        Y_mat(i) = cone.points[i].intensity;
-      }
-
-      // solve ordinary least squares minimisation
-      Eigen::VectorXd solution = X_mat.colPivHouseholderQr().solve(Y_mat);
-
-      // determine colour
-      if (solution(0) > 0)
-        cone_check.color = 1;
-      else
-        cone_check.color = 0;
-      return cone_check;
-    }
-  }
-
-  cone_check.is_cone = false;
-  return cone_check;
-}
-
-/**
- * @brief C++17 hypot extension for 3D coordinates
- *
- */
-float ConeClustering::hypot3d(float a, float b, float c) {
-  return std::sqrt(a * a + b * b + c * c);
-}
-
-
-bool ConeClustering::checkShape(pcl::PointCloud<pcl::PointXYZINormal> cone, Eigen::Vector4f centroid, bool orange){
-  //compute cone model(center + startinglocation)
-  ConeModel cone_model;
-  cone_model.floor = cone.points[0].curvature;
-
-  if(orange){
-    cone_model.height_cone = 0.505;
-    cone_model.half_width_cone = 0.142;
-  }
-
-  double angle = std::atan2(centroid[1], centroid[0]);
-  double translation = cone_model.half_width_cone/3;
-  int sign = (centroid[1] > 0) ? 1 : ((centroid[1] <= 0) ? -1 : 0);
-  cone_model.x = centroid[0] + std::cos(angle)*translation;
-  cone_model.y = centroid[1] + std::sin(angle)*translation*sign;
-
-  double sum = 0;
-  for (pcl::PointXYZINormal point : cone.points){
-    double distance = std::sqrt(std::pow(point.x - cone_model.x,2) + std::pow(point.y - cone_model.y,2));
-    double expected_height = cone_model.height_cone*(1 - distance/cone_model.half_width_cone);
-    double error = std::min(std::abs(1 - ((point.z - point.curvature)/expected_height)), 1.0);
-    sum += 1 - error;
-  }
-
-  double cone_metric = sum/cone.points.size();
-
-  return cone_metric > cone_shape_factor_;
-
-}
 } // namespace ns_lidar
