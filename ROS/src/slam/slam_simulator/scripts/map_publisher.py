d01c037c48c14f9cebfe7b941a260b40e01b9b90
Lowiek <Lowiek@fablabfactory.com>
2022-11-17 15:11:36 +0100

    Added map publisher

---
+++ ROS/src/slam/slam_simulator/scripts/map_publisher.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import yaml
+import rospy
+from ugr_msgs.msg import ObservationWithCovarianceArrayStamped
+
+from genpy.message import fill_message_args
+
+
+class MapPublisher():
+    def __init__(self):
+
+        rospy.init_node("slam_simulator_map_publisher")
+
+        self.map = rospy.get_param(
+            "~map", "/home/lowiek/autonomous2023/ROS/src/slam/slam_simulator/maps/rc_gt1.yaml")
+        self.override_time = rospy.get_param("~override_time", True)
+
+        self.map_publisher = rospy.Publisher(
+            "/output/map",
+            ObservationWithCovarianceArrayStamped,
+            queue_size=1,
+            latch=True,
+        )
+
+        try:
+            self.publish_map()
+        except:
+            rospy.logerr(
+                f"Error publishing map. Make sure that the file '{self.map}' exists, is readable and is valid YAML!")
+
+        rospy.spin()
+
+    def publish_map(self):
+
+        # Try to parse YAML
+        ros_map = ObservationWithCovarianceArrayStamped()
+        with open(self.map, 'r') as file:
+            map = yaml.safe_load(file)
+            fill_message_args(ros_map, map)
+
+
+node = MapPublisher()
