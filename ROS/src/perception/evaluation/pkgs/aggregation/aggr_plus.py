625471064f601b20ca15176ba91dd680cbc1d932
tristan279 <tristandhoedt@gmail.com>
2023-03-27 10:22:15 +0200

    linting

--- ROS/src/perception/evaluation/pkgs/aggregation/aggr_plus.py
+++ ROS/src/perception/evaluation/pkgs/aggregation/aggr_plus.py
@@ -1,13 +1,33 @@
-from aggregation.aggregation import Aggregation
+from typing import Any
+
+import matplotlib.pyplot as plt
+import numpy as np
 from std_msgs.msg import Float32
 
+from aggregation.aggregation import Aggregation
+
+
 class plus_aggr(Aggregation):
+    """
+    example aggregation class
+    publishes the sum of two outputs
+    """
+
     def __init__(self):
         super().__init__("plus")
 
-    def aggregate_data(self,x: Float32,y:Float32):
-        return Float32(data=x.data+y.data)
-    
+    def aggregate_data(self, x: Float32, y: Float32):
+        return Float32(data=x.data + y.data)
+
     def get_output_type(self):
         return Float32
-        
+
+    def plot(self, values, savedir):
+        p = []
+        for v in values:
+            p.append(v.data)
+        plt.plot(np.arange(len(p)), p)
+        plt.savefig(savedir)
+
+    def update_state(self, input: Any) -> Any:
+        return super().update_state(input)
--- ROS/src/perception/evaluation/pkgs/aggregation/aggregation.py
+++ ROS/src/perception/evaluation/pkgs/aggregation/aggregation.py
@@ -1,22 +1,50 @@
 import inspect
-from abc  import ABC,abstractmethod
-from typing import Any,get_type_hints
+from abc import ABC, abstractmethod
+from typing import Any, get_type_hints
+
+
 class Aggregation(ABC):
     """
     Class to include your aggregation functions
     these should be of the form do_{agg_string}
     """
-    def __init__(self,name) -> None:
+
+    def __init__(self, name) -> None:
         self.name = name
+        self.state = None
+
     @abstractmethod
-    def aggregate_data(self):
+    def aggregate_data(self) -> Any:
+        """
+        function that computes the outcome value, should have the right arguments
+        """
         pass
 
     @abstractmethod
-    def get_output_type(self):
+    def get_output_type(self) -> Any:
+        """
+        Needed for the publisher
+        """
         pass
 
-    def get_attrs(self,func:str) -> dict:
+    @abstractmethod
+    def plot(self, values: list[Any], savedir: str) -> None:
+        """
+        Custom plot function
+        args:
+            values: a list of aggregate values that are kept in the plottingnode
+            savedir: the file to save the plot to
+        """
+        pass
+
+    @abstractmethod
+    def update_state(self, input: Any) -> Any:
+        """
+        Update internal state if needed
+        """
+        return self.state
+
+    def get_attrs(self, func: str) -> dict:
         """
         Get the argument names of a function
         Args:
@@ -29,10 +57,10 @@ class Aggregation(ABC):
 
         # check if string is valid
         if hasattr(self, do) and callable(func := getattr(self, do)):
-            
-            return get_type_hints(getattr(self,do))
-        
-    def apply(self,func:str,kwargs:dict)-> Any:
+
+            return get_type_hints(getattr(self, do))
+
+    def apply(self, func: str, kwargs: dict) -> Any:
         """
         applies the aggregation function
         Args:
@@ -46,4 +74,6 @@ class Aggregation(ABC):
         do = f"aggregate_data"
         # check if string is valid
         if hasattr(self, do) and callable(func := getattr(self, do)):
-            return getattr(self,do)(**kwargs)
+            value = getattr(self, do)(**kwargs)
+            self.update_state(value)
+            return value
