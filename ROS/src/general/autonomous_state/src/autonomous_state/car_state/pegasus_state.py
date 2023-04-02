d9e22c4323f8b968508826554f326d8dc849915b
Lowiek <Lowiek@fablabfactory.com>
2023-04-02 18:58:08 +0200

    Fixed bugs in pegasus state

--- ROS/src/general/autonomous_state/src/autonomous_state/car_state/pegasus_state.py
+++ ROS/src/general/autonomous_state/src/autonomous_state/car_state/pegasus_state.py
@@ -3,8 +3,6 @@ from can_msgs.msg import Frame
 from car_state import carStateEnum, CarState
 from node_fixture import AutonomousStatesEnum, AutonomousMission
 import can
-from nav_msgs.msg import Odometry
-from fs_msgs.msg import ControlCommand
 import rosparam
 
 class PegasusState(CarState):
@@ -28,9 +26,6 @@ class PegasusState(CarState):
 
         self.as_ready_time = rospy.Time.now().to_sec()
 
-        self.odom = Odometry()
-        self.control = ControlCommand()
-
         self.state = {
             "TS": carStateEnum.UNKOWN,
             "ASMS": carStateEnum.UNKOWN,
@@ -52,18 +47,20 @@ class PegasusState(CarState):
         Handles incoming CAN message, but as a subscriber callback
         This way, we can put all HW/SW interfacing code in a single CAN driver.
 
-        TS: 
+        TS:
             We consider the TS to be 'on' if the ODrive sends heartbeat messages
-        
+
         ASB:
-            We don't have actual brakes, so the ASB is considered to be 'activated' when
+            We don't have actual brakes, so the ASB is considered to be 'on' or 'activated' when
             the ODrive sends heartbeat messages
+
+            If we receive odrive heartbeat, ASB is 'activated' if AS state is 'ASOff', else it is 'on'.
 
         ASMS:
             The switch in the back. The Teensy reports the status of the switch
 
         R2D: (T 14.9.3)
-            Is only set to 'on' when AS is at least 5 seconds in ASReady and 
+            Is only set to 'on' when AS is at least 5 seconds in ASReady and
             the RES reports 'Go' being pressed
 
         EBS:
@@ -72,12 +69,12 @@ class PegasusState(CarState):
 
             If the E-stop of the RES has been triggered, the power will be cut, which also means the EBS
             has been triggered.
-            
-            Triggering EBS from within the AS is not possible in Pegasus. 
+
+            Triggering EBS from within the AS is not possible in Pegasus.
 
             Disabling the EBS ('off') is also not possible. EBS is either 'on' (armed) or 'activated'
         """
-        
+
         # ODrive
         axis_id = frame.id >> 5
         if axis_id == 1 or axis_id == 2:
@@ -86,13 +83,13 @@ class PegasusState(CarState):
             # Heartbeat message odrive
             if cmd_id == 1:
                 self.odrive_hb = rospy.Time.now().to_sec()
-        
+
         # RES
         if frame.id == 0x191:
             self.res_go_signal = (frame.data[0] & 0b0000100) >> 2
-            self.res_estop_signal = (frame.data[0] & 0b0000001)
-        
-        # Teensy 
+            self.res_estop_signal = frame.data[0] & 0b0000001
+
+        # Teensy
         node_id = frame.id >> 5
         if node_id == 0xE0:
             cmd_id = frame.id & 0b11111
@@ -108,16 +105,19 @@ class PegasusState(CarState):
     def update(self, state: AutonomousStatesEnum):
 
         # On a state transition, start 5 second timeout
-        if state == AutonomousStatesEnum.ASREADY and self.as_state != AutonomousStatesEnum.ASREADY:
+        if (
+            state == AutonomousStatesEnum.ASREADY
+            and self.as_state != AutonomousStatesEnum.ASREADY
+        ):
             self.as_ready_time = rospy.Time.now().to_sec()
 
         self.as_state = state
 
-    
+        self.send_status_over_can()
+
     def send_status_over_can(self):
         # https://www.formulastudent.de/fileadmin/user_upload/all/2022/rules/FSG22_Competition_Handbook_v1.1.pdf
 
-        
         # 0x500 whatever
         # 0x501 whatever
 
@@ -149,11 +149,13 @@ class PegasusState(CarState):
         elif self.state["EBS"] == carStateEnum.ACTIVATED:
             bits = 3
 
-        data[0] |= bits << 3 
+        data[0] |= bits << 3
 
         # Bits 5 - 7
         bits = 0
-        mission = rosparam.get_param("/mission") if rosparam.has_param("/mission") else ""
+        mission = (
+            rosparam.get_param("/mission") if rosparam.has_param("/mission") else ""
+        )
 
         if mission == AutonomousMission.ACCELERATION:
             bits = 1
@@ -169,7 +171,7 @@ class PegasusState(CarState):
             bits = 6
 
         data[0] |= bits << 5
-        
+
         # De rest whatever
 
         canmsg = can.Message(
@@ -178,7 +180,6 @@ class PegasusState(CarState):
             is_extended_id=False,
         )
         self.bus.send(canmsg)
-
 
     def get_state(self):
         """
@@ -196,10 +197,24 @@ class PegasusState(CarState):
             self.state["R2D"] = carStateEnum.OFF
 
         # TS and ASB
-        self.state["TS"] = t - self.odrive_hb < 0.2
-        self.state["ASB"] = t - self.odrive_hb < 0.2
+        self.state["TS"] = (
+            carStateEnum.ON if t - self.odrive_hb < 0.2 else carStateEnum.OFF
+        )
+        self.state["ASB"] = (
+            (
+                carStateEnum.ACTIVATED
+                if self.as_state == AutonomousStatesEnum.ASOFF
+                else carStateEnum.ON
+            )
+            if t - self.odrive_hb < 0.2
+            else carStateEnum.OFF
+        )
 
         # EBS
-        self.state["EBS"] = carStateEnum.ACTIVATED if self.res_estop_signal or t - self.teensy_hb < 0.2 else carStateEnum.ON
+        self.state["EBS"] = (
+            carStateEnum.ACTIVATED
+            if self.res_estop_signal or t - self.teensy_hb < 0.2
+            else carStateEnum.ON
+        )
 
         return self.state
