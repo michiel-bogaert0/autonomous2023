4bc7e184ec2584867503265f3a22bb0e454ca120
ZanderDaninck <zander.daninck@gmail.com>
2023-07-29 22:29:54 +0200

    utils

---
+++ ROS/src/control/pathplanning/include/utils.hpp
@@ -0,0 +1,31 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <vector>
+#include <array>
+
+namespace triangulation {
+struct Node {
+    double x;
+    double y;
+    double distance;
+    Node* parent; // Pointer to the parent node
+    std::vector<Node*> children; // Vector of pointers to child nodes
+    double angle;
+    double angle_change;
+};
+
+// Define a utility function to calculate squared distance between 2 points
+double distance_squared(double x1, double y1, double x2, double y2);
+
+// Function to check if there is no collision between a node and a point with safety distance around cones
+bool no_collision(const Node& parent, const std::array<double, 2>& point, const std::vector<std::array<double, 3>>& cones, double safety_dist_squared);
+
+// Function to get the closest center points to an origin point
+std::vector<std::array<double, 2>> get_closest_center(const std::vector<std::array<double, 2>>& center_points, size_t amount, const std::array<double, 2>& origin = {0, 0}, double max_distance = -1);
+
+// Function to sort the center points based on their distance to the origin point
+std::vector<std::array<double, 2>> sort_closest_to(const std::vector<std::array<double, 2>>& center_points, const std::array<double, 2>& origin = {0, 0}, double max_distance = -1);
+} // namespace triangulation
+
+#endif // UTILS_HPP
---
+++ ROS/src/control/pathplanning/src/utils.cpp
@@ -0,0 +1,127 @@
+#include <vector>
+#include <array>
+#include <cmath>
+#include <algorithm>
+
+namespace triangulation {
+
+double distance_squared(double x1, double y1, double x2, double y2)
+{
+    return std::pow(x1 - x2, 2) + std::pow(y1 - y2, 2);
+}
+
+bool no_collision(const Node& parent, const std::array<double, 2>& point, const std::vector<std::array<double, 3>>& cones, double safety_dist_squared)
+{
+    double x1 = parent.x;
+    double y1 = parent.y;
+    double x2 = point[0];
+    double y2 = point[1];
+
+    for (const auto& cone : cones)
+    {
+        double xc = cone[0];
+        double yc = cone[1];
+
+        double t_numerator = (x1 - xc) * (x1 - x2) + (y1 - yc) * (y1 - y2);
+        double t_denominator = std::pow(x1 - x2, 2) + std::pow(y1 - y2, 2);
+
+        double t = t_numerator / t_denominator;
+        t = std::max(0.0, std::min(1.0, t));
+
+        double xp = x1 + (x2 - x1) * t;
+        double yp = y1 + (y2 - y1) * t;
+
+        double dist_squared = distance_squared(xc, yc, xp, yp);
+        if (dist_squared < safety_dist_squared)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::vector<std::array<double, 2>> get_closest_center(const std::vector<std::array<double, 2>>& center_points, size_t amount, const std::array<double, 2>& origin, double max_distance)
+{
+    std::vector<double> distances_squared;
+    distances_squared.reserve(center_points.size());
+
+    for (const auto& center_point : center_points)
+    {
+        double dist_squared = distance_squared(origin[0], origin[1], center_point[0], center_point[1]);
+        distances_squared.push_back(dist_squared);
+    }
+
+    std::vector<std::array<double, 2>> points_within_distance;
+    for (size_t i = 0; i < center_points.size(); i++)
+    {
+        if (distances_squared[i] < max_distance * max_distance || max_distance == -1)
+        {
+            points_within_distance.push_back(center_points[i]);
+        }
+    }
+
+    // If there are not enough points
+    if (points_within_distance.size() <= amount)
+    {
+        return points_within_distance;
+    }
+
+    // Get the 'amount' closest center points
+    std::vector<size_t> ind(center_points.size());
+    std::iota(ind.begin(), ind.end(), 0);
+
+    std::partial_sort(ind.begin(), ind.begin() + amount, ind.end(), [&distances_squared](size_t i1, size_t i2) {
+        return distances_squared[i1] < distances_squared[i2];
+    });
+
+    std::vector<std::array<double, 2>> closest_center_points;
+    closest_center_points.reserve(amount);
+    for (size_t i = 0; i < amount; i++)
+    {
+        closest_center_points.push_back(center_points[ind[i]]);
+    }
+
+    return closest_center_points;
+}
+
+std::vector<std::array<double, 2>> sort_closest_to(const std::vector<std::array<double, 2>>& center_points, const std::array<double, 2>& origin, double max_distance)
+{
+    std::vector<double> distances_squared;
+    distances_squared.reserve(center_points.size());
+
+    for (const auto& center_point : center_points)
+    {
+        double dist_squared = distance_squared(origin[0], origin[1], center_point[0], center_point[1]);
+        distances_squared.push_back(dist_squared);
+    }
+
+    std::vector<std::array<double, 2>> points_within_distance;
+    std::vector<double> distances_squared_points_within_distance;
+    for (size_t i = 0; i < center_points.size(); i++)
+    {
+        if (distances_squared[i] < max_distance * max_distance || max_distance == -1)
+        {
+            points_within_distance.push_back(center_points[i]);
+            distances_squared_points_within_distance.push_back(distances_squared[i]);
+        }
+    }
+
+    std::vector<size_t> ind(points_within_distance.size());
+    std::iota(ind.begin(), ind.end(), 0);
+
+    std::sort(ind.begin(), ind.end(), [&distances_squared_points_within_distance](size_t i1, size_t i2) {
+        return distances_squared_points_within_distance[i1] < distances_squared_points_within_distance[i2];
+    });
+
+    std::vector<std::array<double, 2>> sorted_points_within_distance;
+    sorted_points_within_distance.reserve(points_within_distance.size());
+    for (size_t i = 0; i < points_within_distance.size(); i++)
+    {
+        sorted_points_within_distance.push_back(points_within_distance[ind[i]]);
+    }
+
+    return sorted_points_within_distance;
+}
+
+} // namespace triangulation
