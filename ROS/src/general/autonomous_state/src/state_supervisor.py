e8e187494fd132ec1c29726fe279ad12a69c38a2
Lowiek <Lowiek@fablabfactory.com>
2023-09-09 16:25:14 +0200

    Added and updated workflows

---
+++ ROS/src/general/autonomous_state/src/state_supervisor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import rospy
+import sys
+from ugr_msgs.msg import State
+from node_fixture.node_fixture import AutonomousStatesEnum, StateMachineScopeEnum
+
+class StateSupervisorNode:
+    def __init__(self):
+        rospy.init_node('state_supervisor', anonymous=True)
+
+        # Get parameters for the timeout and topic to monitor
+        self.timeout = rospy.get_param('~timeout', 10.0)  # Default timeout is 10 seconds
+
+        # Initialize variables
+        self.state = AutonomousStatesEnum.ASOFF
+        self.start_time = None
+        self.rate = rospy.Rate(5)
+
+        # Subscribe to the topic
+        rospy.Subscriber("/state", State, self.state_callback)
+
+    def state_callback(self, data: State):
+        
+        if data.scope == StateMachineScopeEnum.AUTONOMOUS:
+            self.state = data.cur_state
+
+    def run(self):
+        self.start_time = rospy.Time.now().to_sec()
+        rospy.loginfo(f"Monitoring state topic for {AutonomousStatesEnum.ASFINISHED} with a timeout of {self.timeout} seconds.")
+
+        while not rospy.is_shutdown():
+            if self.state == AutonomousStatesEnum.ASFINISHED:
+                
+                rospy.loginfo(f"{AutonomousStatesEnum.ASFINISHED} received in time. Exiting successfully.")
+                rospy.signal_shutdown("Result received successfully")
+                break
+
+            elif self.state == AutonomousStatesEnum.ASEMERGENCY:
+
+                rospy.logerr(f"{AutonomousStatesEnum.ASEMERGENCY} received. Exiting with an error")
+                raise rospy.ROSException(f"Received {AutonomousStatesEnum.ASEMERGENCY}")
+
+            elif (rospy.Time.now().to_sec() - self.start_time) > self.timeout:
+            
+                rospy.logerr(f"Timeout reached. {AutonomousStatesEnum.ASFINISHED} not received. Exiting with an error.")
+                raise rospy.ROSException(f"Timeout reached")
+            
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        node = StateSupervisorNode()
+        node.run()
+    except rospy.ROSException:
+        sys.exit(1)
