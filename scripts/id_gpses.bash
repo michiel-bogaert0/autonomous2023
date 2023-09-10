bd19d4274496919a3d0e4fb6bebffb5edf31f0b2
Lowiek <Lowiek@fablabfactory.com>
2023-09-11 01:03:00 +0200

    Updated ipc startup script

--- scripts/id_gpses.bash
+++ scripts/id_gpses.bash
@@ -5,23 +5,30 @@ L="1-2"
 # echo USB Devices:
 # ls /dev/ttyUSB* | cut -d "/" -f 3
 
-output="export UGR_CAR=pegasus\n"
+output=""
 
 for usb in $(ls /dev/ttyUSB* | cut -d "/" -f 3)
 do
   cd -P /sys/class/tty/$usb/device
   path=$(pwd)
   if [[ $path =~ $R ]]; then
-    output+="export MOVING_ROVER_USB=/dev/$usb\n"
+    sudo /home/$(whoami)/autonomous2023/run set-env MOVING_ROVER_USB $usb
     echo "Right (moving rover): $usb"
   fi
   if [[ $path =~ $L ]]; then
-    output+="export FIXED_ROVER_USB=/dev/$usb\n"
+    sudo /home/$(whoami)/autonomous2023/run set-env FIXED_ROVER_USB $usb
     echo "Left (fixed rover): $usb"
   fi
 done
 
-sudo echo -e "#!/bin/bash\n\n$output" > /home/ugentracing/autonomous2023/env-vars.sh
-chmod +x /home/ugentracing/autonomous2023/env-vars.sh
+sudo /home/$(whoami)/autonomous2023/run set-car pegasus
+sudo /home/$(whoami)/autonomous2023/run set-env ROS_HOSTNAME 192.168.50.17
+sudo /home/$(whoami)/autonomous2023/run set-env ROS_MASTER_URI http://192.168.50.17:11311
 
-echo "Environment variable export script written to /home/ugentracing/autonomous2023/env-vars.sh"
+chmod +x /home/$(whoami)/autonomous2023/env-vars.sh
+
+echo "Environment variable export script written to /home/$(whoami)/autonomous2023/env-vars.sh"
+
+sudo /home/$(whoami)/autonomous2023/run stop
+sudo /home/$(whoami)/autonomous2023/run start-headless
+sudo /home/$(whoami)/autonomous2023/run pegasus
