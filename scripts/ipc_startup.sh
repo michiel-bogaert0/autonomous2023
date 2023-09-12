29009aa79e71e69f77efe33fe9ca8260668c8110
iPC <iPC@ugentracing.be>
2023-09-12 13:06:30 +0000

    Fixed bugs

--- scripts/ipc_startup.sh
+++ scripts/ipc_startup.sh
@@ -27,15 +27,38 @@ if [[ $path =~ $L ]]; then
 fi
 done
 
-bash /home/ugentracing/autonomous2023/scripts/id_gpses.bash
+# Setup environment
+R="1-1"
+L="1-2"
 
-# Setup environment and start code
-sudo /home/$(whoami)/autonomous2023/run set-car pegasus
-sudo /home/$(whoami)/autonomous2023/run set-env ROS_HOSTNAME 192.168.50.17
-sudo /home/$(whoami)/autonomous2023/run set-env ROS_MASTER_URI http://192.168.50.17:11311
+# echo USB Devices:
+ls /dev/ttyUSB* | cut -d "/" -f 3
 
-sudo /home/$(whoami)/autonomous2023/run stop
-sudo /home/$(whoami)/autonomous2023/run start-headless
-sudo /home/$(whoami)/autonomous2023/run pegasus
+for usb in $(ls /dev/ttyUSB* | cut -d "/" -f 3)
+do
+  cd -P /sys/class/tty/$usb/device
+  path=$(pwd)
+  if [[ $path =~ $R ]]; then
+    cd /home/ugentracing/autonomous2023
+    /home/ugentracing/autonomous2023/run set-env MOVING_ROVER_USB $usb
+    echo "Right (moving rover): $usb"
+  fi
+  if [[ $path =~ $L ]]; then
+    cd /home/ugentracing/autonomous2023
+    /home/ugentracing/autonomous2023/run set-env FIXED_ROVER_USB $usb
+    echo "Left (fixed rover): $usb"
+  fi
+done
+
+cd /home/ugentracing/autonomous2023
+
+/home/ugentracing/autonomous2023/run set-car pegasus
+/home/ugentracing/autonomous2023/run set-env ROS_HOSTNAME 192.168.50.17
+/home/ugentracing/autonomous2023/run set-env ROS_MASTER_URI http://192.168.50.17:11311
+
+# Start code
+/home/ugentracing/autonomous2023/run stop
+/home/ugentracing/autonomous2023/run start-headless
+/home/ugentracing/autonomous2023/run pegasus
 
 exit 0
