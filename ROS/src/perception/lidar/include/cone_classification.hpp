70c0a9a1c1db2ad25e6c82b3f79d7bec09ec1be6
Robbe1612 <robbegalle1@gmail.com>
2023-10-03 17:14:07 +0200

    lidar orange cone removal

--- ROS/src/perception/lidar/include/cone_classification.hpp
+++ ROS/src/perception/lidar/include/cone_classification.hpp
@@ -49,6 +49,7 @@ private:
                              // 1 -> the pointcloud must have the precise shape
                              // of a cone
   double height_width_factor_;   // the minimal value height/depth needs to have
+  bool use_orange_cones_;        // are orange cones used or not
   bool use_white_cones_;         // are white cones passed forward or not
   double threshold_white_cones_; // distance where the lidar will stop
                                  // determining color and set the color to white
