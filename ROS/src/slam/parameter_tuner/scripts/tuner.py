4a64683826ebf64c05c0f3935ccc0ab0953a79b7
Lowie Slachmuylders <lowie.slachmuylders@ugentracing.be>
2023-10-14 20:28:58 +0200

    add launch car

--- ROS/src/slam/parameter_tuner/scripts/tuner.py
+++ ROS/src/slam/parameter_tuner/scripts/tuner.py
@@ -11,36 +11,57 @@ class Tuner:
         """
         rospy.init_node("parameter_tuner")
 
-        self.filename = rospy.get_param('~filename', "circle_R15.yaml")
+        self.map_filename = rospy.get_param('~map', "circle_R15") + ".yaml"
+        self.mission = rospy.get_param('~mission', "trackdrive")
 
         self.simulationLauncher = NodeLauncher()
         self.carLauncher = NodeLauncher()
         
-        self.launch_simulation(self.filename)
+        self.launch_simulation(self.map_filename)
+
+        sleep(4)
+
+        self.launch_car(self.mission)
 
         while not rospy.is_shutdown():
             
 
             sleep(0.1)
 
+        self.carLauncher.shutdown()
         self.simulationLauncher.shutdown()
 
-
     #
-    def launch_simulation(self, filename: str):
+    def launch_simulation(self, map_filename: str):
         """
-        Launches the simulation launchfile using the provided filename parameter.
+        Launches the simulation launchfile using the provided map_filename parameter.
 
         Args:
-            filename (str): The filename to be passed as a parameter to the simulation launch file.
+            map_filename (str): The filename to be passed as a parameter to the simulation launch file.
 
         """
         rospy.loginfo("Launch simulation")
-        self.simulationLauncher.launch_node("ugr_launch", "launch/external/simulation.launch", [str('filename:=' + filename)])
+        self.simulationLauncher.launch_node("ugr_launch", "launch/external/simulation.launch", [str('filename:=' + map_filename)])
         try:
             self.simulationLauncher.run()
         except Exception as e:
             rospy.logerr("Simulation launch: %s", str(e))
+
+    def launch_car(self, mission: str):
+        """
+        Launches the run launchfile using the provided mission parameter.
+
+        Args:
+            mission (str): The mission to be passed as a parameter to the run launch file.
+
+        """
+        rospy.loginfo("Launch car run")
+        self.carLauncher.launch_node("ugr_launch", "launch/internal/run.launch", [str('mission:=' + mission)])
+        try:
+            self.carLauncher.run()
+        except Exception as e:
+            rospy.logerr("Car run launch: %s", str(e))
+
 
 
 node = Tuner()
