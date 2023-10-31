81f9ad6d82c7711720571631b76118b709633168
Lowie Slachmuylders <slachmuylders.lowie@gmail.com>
2023-10-31 17:18:25 +0100

    add list tuner

--- ROS/src/slam/parameter_tuner/scripts/main.py
+++ ROS/src/slam/parameter_tuner/scripts/main.py
@@ -5,7 +5,7 @@ import rospy
 from data import Data, SimulationData, SimulationStopEnum
 from launcher import Launcher
 from node_fixture.node_fixture import AutonomousStatesEnum, StateMachineScopeEnum
-from param import TunerModes, getTuner
+from param import getTuner
 from std_msgs.msg import UInt16
 from ugr_msgs.msg import State
 
@@ -19,16 +19,21 @@ class Main:
 
         self.map_filename = rospy.get_param("~map", "circle_R15") + ".yaml"
         self.mission = rospy.get_param("~mission", "trackdrive")
+        self.tuner_mode = rospy.get_param("~tuner_mode", "none")
         self.max_time = rospy.Duration.from_sec(rospy.get_param("~max_time", 60))
         self.number_of_simulations = rospy.get_param("~number_of_simulations", 2)
 
         self.state = AutonomousStatesEnum.ASOFF
 
         self.launcher = Launcher(rospy.get_param("~logging", True))
-        self.param = getTuner(TunerModes.SUM)(
-            rospy.get_param("~yaml_file_path"), rospy.get_param("~parameter"), 100
+        self.param = getTuner(self.tuner_mode)(
+            rospy.get_param("~yaml_file_path"), rospy.get_param("~parameter")
         )
         self.saveData = Data()
+
+        # If the tuner mode is list set the number simulations to the lenght of the list
+        if self.tuner_mode == "list":
+            self.number_of_simulations = self.param.get_number_of_simulations()
 
         # Subscribers
         rospy.Subscriber("/state", State, self.state_callback)
@@ -41,34 +46,26 @@ class Main:
             # Stops the simulation if the car of the car is finished
             if self.state == AutonomousStatesEnum.ASFINISHED:
                 rospy.loginfo("Simulation is finisched")
-                self.new_simulation(SimulationStopEnum.ASFINISHED)
+                self.stop_and_start_simulation(SimulationStopEnum.ASFINISHED)
 
             # Stops the simulation if the duration of the simulation is longer than the max time
             if rospy.Time.now() - self.start_time > self.max_time:
                 rospy.loginfo("Simulation out of time")
-                self.new_simulation(SimulationStopEnum.TimeLimit)
+                self.stop_and_start_simulation(SimulationStopEnum.TimeLimit)
 
             sleep(0.1)
 
         self.launcher.shutdown()
 
-    def state_callback(self, data: State):
-        rospy.loginfo("Scope: %s, State: %s", data.scope, data.cur_state)
-        if data.scope == StateMachineScopeEnum.AUTONOMOUS:
-            self.state = data.cur_state
-
-    def loopclosure_callback(self, data):
-        rospy.loginfo("Laps finished: %s", data.data)
-
-    def new_simulation(self, stop_reason: str) -> None:
+    def stop_and_start_simulation(self, stop_reason: str) -> None:
         self.launcher.shutdown()
 
         # Save simulation data
         self.simulation_data.duration = rospy.Time.now() - self.start_time
         self.simulation_data.stop_reason = stop_reason
-
         self.saveData.add(self.simulation_data)
 
+        # Did all the simulations
         if self.simulation >= self.number_of_simulations:
             rospy.loginfo("Finished all simulations")
 
@@ -82,17 +79,22 @@ class Main:
     def start_simulation(self):
         self.state = AutonomousStatesEnum.ASOFF
         parameter = self.param.change()
-
         sleep(1)
-
         self.simulation += 1
         rospy.loginfo("Start simulation %s", self.simulation)
         self.simulation_data = SimulationData(self.simulation, parameter)
-
         self.launcher.launch_simulation(self.map_filename)
         sleep(1)
         self.launcher.launch_car(self.mission)
         self.start_time = rospy.Time.now()
+
+    def state_callback(self, data: State):
+        rospy.loginfo("Scope: %s, State: %s", data.scope, data.cur_state)
+        if data.scope == StateMachineScopeEnum.AUTONOMOUS:
+            self.state = data.cur_state
+
+    def loopclosure_callback(self, data):
+        rospy.loginfo("Laps finished: %s", data.data)
 
 
 node = Main()
--- ROS/src/slam/parameter_tuner/scripts/param.py
+++ ROS/src/slam/parameter_tuner/scripts/param.py
@@ -7,11 +7,14 @@ import yaml
 class TunerModes(str, enum.Enum):
     NONE = "none"
     SUM = "sum"
+    LIST = "list"
 
 
 def getTuner(mode: TunerModes):
     if mode == TunerModes.SUM:
         return SumParam
+    elif mode == TunerModes.LIST:
+        return ListParam
     else:
         return Param
 
@@ -27,15 +30,18 @@ class Param:
         self.previous = self.get_parameter()
 
     def change(self):
-        self.set_parameter(self.change_tuner(self.previous))
+        self.previous = self.set_parameter(self.change_tuner(self.previous))
         return self.previous
 
+    # change the value of the parameter for the next simulation run
     def change_tuner(self, p):
         return p
 
+    # get the value from the parameter
     def get_parameter(self):
         return self.data[0][self.parameter_name]
 
+    # Set the new value of the parameter in the yaml file
     def set_parameter(self, parameter):
         self.data[0][self.parameter_name] = parameter
         with open(self.yaml_path, "w") as file:
@@ -44,12 +50,28 @@ class Param:
         rospy.loginfo(
             f"Change parameter({self.parameter_name}) to {parameter} in yaml file"
         )
+        return parameter
 
 
 class SumParam(Param):
-    def __init__(self, yaml_path: str, parameter_name: str, value) -> None:
+    def __init__(self, yaml_path: str, parameter_name: str) -> None:
         Param.__init__(self, yaml_path, parameter_name)
-        self.value = value
+        self.value = rospy.get_param("~sum_add_value", 0)
 
     def change_tuner(self, p):
-        return p - self.value
+        return p + self.value
+
+
+class ListParam(Param):
+    def __init__(self, yaml_path: str, parameter_name: str) -> None:
+        Param.__init__(self, yaml_path, parameter_name)
+        self.list = rospy.get_param("~list", [])
+        self.index = 0
+
+    def change_tuner(self, p):
+        param = self.list[self.index]
+        self.index += 1
+        return param
+
+    def get_number_of_simulations(self):
+        return len(self.list)
