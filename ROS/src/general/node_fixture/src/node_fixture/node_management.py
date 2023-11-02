49258e3f9a8848ce328927d35abaa5ec758d193e
Robbe1612 <robbegalle1@gmail.com>
2023-11-02 10:48:36 +0100

    first version

--- ROS/src/general/node_fixture/src/node_fixture/node_management.py
+++ ROS/src/general/node_fixture/src/node_fixture/node_management.py
@@ -1,62 +1,82 @@
 #!/usr/bin/python3
-import enum
-from collections import deque
-from functools import partial,partialmethod
-from pathlib import Path
-from typing import Any, Type, get_type_hints
 from abc import ABC, abstractmethod
-from node_fixture import NodeManagingStatesEnum
+from functools import partialmethod
 
 import rospy
-import rostopic
-
+from node_fixture import NodeManagingStatesEnum
+from node_fixture.srv import GetNodeState, SetNodeState
 
 
 class ManagedNode(ABC):
-    def __init__(self,name):
+    def __init__(self, name):
         self.state = NodeManagingStatesEnum.UNCONFIGURED
         self.handlerlist = []
         self.publishers = []
-        rospy.Service(f"/node_managing/{name}", ManagedNode, self.handle_service)
+        rospy.Service(
+            f"/node_managing/{name}/set", GetNodeState, self.handle_service_set
+        )
+        rospy.Service(
+            f"/node_managing/{name}/get", SetNodeState, self.handle_service_get
+        )
 
-        
+    def handle_service_get(self):
+        return self.state
 
-    def handle_service(self, request):
-        if (self.state == NodeManagingStatesEnum.UNCONFIGURED and request == NodeManagingStatesEnum.INACTIVE):
+    def handle_service_set(self, request):
+        if (
+            self.state == NodeManagingStatesEnum.UNCONFIGURED
+            and request == NodeManagingStatesEnum.INACTIVE
+        ):
             self.doConfigure()
             self.state = NodeManagingStatesEnum.INACTIVE
-        elif (self.state == NodeManagingStatesEnum.INACTIVE and request == NodeManagingStatesEnum.UNCONFIGURED):
+        elif (
+            self.state == NodeManagingStatesEnum.INACTIVE
+            and request == NodeManagingStatesEnum.UNCONFIGURED
+        ):
             self.doCleanup()
             self.state = NodeManagingStatesEnum.UNCONFIGURED
-        elif (self.state == NodeManagingStatesEnum.INACTIVE and request == NodeManagingStatesEnum.ACTIVE):
+        elif (
+            self.state == NodeManagingStatesEnum.INACTIVE
+            and request == NodeManagingStatesEnum.ACTIVE
+        ):
             self.doActivate()
             self.state = NodeManagingStatesEnum.ACTIVE
-        elif (self.state == NodeManagingStatesEnum.ACTIVE and request == NodeManagingStatesEnum.INACTIVE):
+        elif (
+            self.state == NodeManagingStatesEnum.ACTIVE
+            and request == NodeManagingStatesEnum.INACTIVE
+        ):
             self.doDeactivate()
             self.state = NodeManagingStatesEnum.INACTIVE
-        elif (self.state == NodeManagingStatesEnum.INACTIVE and request == NodeManagingStatesEnum.FINALIZED):
+        elif (
+            self.state == NodeManagingStatesEnum.INACTIVE
+            and request == NodeManagingStatesEnum.FINALIZED
+        ):
             self.doShutdown()
             self.state = NodeManagingStatesEnum.FINALIZED
-        elif (self.state == NodeManagingStatesEnum.ACTIVE and request == NodeManagingStatesEnum.FINALIZED):
+        elif (
+            self.state == NodeManagingStatesEnum.ACTIVE
+            and request == NodeManagingStatesEnum.FINALIZED
+        ):
             self.doShutdown()
             self.state = NodeManagingStatesEnum.FINALIZED
-        elif (self.state == NodeManagingStatesEnum.UNCONFIGURED and request == NodeManagingStatesEnum.FINALIZED):
+        elif (
+            self.state == NodeManagingStatesEnum.UNCONFIGURED
+            and request == NodeManagingStatesEnum.FINALIZED
+        ):
             self.doError()
             self.state = NodeManagingStatesEnum.FINALIZED
 
         for pub in self.publishers:
             pub.set_state(self.state)
-        
-    
 
     @abstractmethod
     def doConfigure():
         pass
-    
+
     @abstractmethod
     def doActivate():
         pass
-    
+
     @abstractmethod
     def doCleanup():
         pass
@@ -72,7 +92,7 @@ class ManagedNode(ABC):
     @abstractmethod
     def doError():
         pass
-    
+
     @abstractmethod
     def Active():
         pass
@@ -90,40 +110,37 @@ class ManagedNode(ABC):
         pass
 
     def update(self):
-        if(self.state==NodeManagingStatesEnum.ACTIVE):
+        if self.state == NodeManagingStatesEnum.ACTIVE:
             self.Active()
-        elif(self.state==NodeManagingStatesEnum.INACTIVE):
+        elif self.state == NodeManagingStatesEnum.INACTIVE:
             self.Inactive()
-        elif(self.state==NodeManagingStatesEnum.UNCONFIGURED):
-            self.Unconfigured
-        elif(self.state==NodeManagingStatesEnum.FINALIZED):
+        elif self.state == NodeManagingStatesEnum.UNCONFIGURED:
+            self.Unconfigured()
+        elif self.state == NodeManagingStatesEnum.FINALIZED:
             self.Finalized()
-    
-
-
 
     def AddSubscriber(self, topic, msg_type, handler):
         our_handler = partialmethod(self._make_custom_handler, handler)
         self.handlerlist.append(our_handler)
         return rospy.Subscriber(topic, msg_type, self.handlerlist[-1])
-        
+
     def _make_custom_handler(self, handler, msg):
         if self.state == NodeManagingStatesEnum.ACTIVE:
             return handler(msg)
-    
+
     def AddPublisher(self, topic, msg_type, queue_size):
-        custompublisher = CustomPublisher(topic, msg_type, queue_size,self.state)
+        custompublisher = CustomPublisher(topic, msg_type, queue_size, self.state)
         self.publishers.append(custompublisher)
         return custompublisher
 
 
-#override the publish method of rospy.Publisher
+# override the publish method of rospy.Publisher
 class CustomPublisher(rospy.Publisher):
     def __init__(self, topic, msg_type, queue_size, state):
         super(CustomPublisher, self).__init__(topic, msg_type, queue_size)
         self.state = state
 
-    def set_state(self, state):    
+    def set_state(self, state):
         self.state = state
 
     def publish(self, msg):
