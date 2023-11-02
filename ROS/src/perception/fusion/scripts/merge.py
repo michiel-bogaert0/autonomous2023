bb4958e2d5957058e4e1e118a59ca7df16cd788e
michiel-bogaert0 <michielbogaert.mb13@gmail.com>
2023-11-02 13:14:29 +0100

    fusion kdtree v1.0

--- ROS/src/perception/fusion/scripts/merge.py
+++ ROS/src/perception/fusion/scripts/merge.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import TransformStamped
-from ugr_msgs.msg import ObservationWithCovarianceArrayStamped
+from ugr_msgs.msg import (
+    ObservationWithCovariance,
+    ObservationWithCovarianceArrayStamped
+)
 import tf2_ros as tf
 from node_fixture.node_fixture import ROSNode
 from sklearn.neighbors import KDTree
@@ -40,7 +43,7 @@ class MergeNode:
         self.camera_last_obs_time, self.lidar_last_obs_time = 0.0, 0.0
         self.waiting_time_ms = 50
 
-        self.merge_distance_threshold = 4.0
+        self.merge_distance_threshold = 2.5               # set threshold max fusion distance
 
         # Random helpers
         self.is_first_received = True
@@ -104,7 +107,7 @@ class MergeNode:
             self.last_received_sensor = observations.header.frame_id
             return
         
-        if second_observations == None:
+        if second_observations is None:
             rospy.logwarn(f"Did not find second observations, publishing {observations.header.frame_id}")
             self.publish(observations)
             self.is_first_received = True
@@ -119,6 +122,9 @@ class MergeNode:
         """
         * * * FUNCTION STILL INCOMPLETE (AND PROBABLY INCORRECT)! * * *
         """
+
+        # Get covariance arrays of all observations
+        early_covariances, late_covariances = [observation.covariance for observation in early_obs.observations], [observation.covariance for observation in late_obs.observations]
 
         try:
             tf_early_to_base: TransformStamped = self.tf_buffer.lookup_transform_full(
@@ -140,12 +146,17 @@ class MergeNode:
                 )
             
             
-            time_transformed_early_obs = ROSNode.do_transform_observations(
+            time_transformed_early_obs: ObservationWithCovarianceArrayStamped = ROSNode.do_transform_observations(
                     early_obs, tf_early_to_base
             )
-            time_transformed_late_obs = ROSNode.do_transform_observations(
+            time_transformed_late_obs: ObservationWithCovarianceArrayStamped = ROSNode.do_transform_observations(
                     late_obs, tf_late_to_base
             )
+
+            for i in range(len(time_transformed_early_obs.observations)):
+                time_transformed_early_obs.observations[i].covariance = early_covariances[i]
+            for i in range(len(time_transformed_late_obs.observations)):
+                time_transformed_late_obs.observations[i].covariance = late_covariances[i]
             
             self.kd_tree_merger(time_transformed_early_obs, time_transformed_late_obs)
 
@@ -156,13 +167,26 @@ class MergeNode:
             )
 
     def kd_tree_merger(self, observations1, observations2):
+        rospy.loginfo(observations1)
         all_observations = observations1.observations + observations2.observations
-        points = list(map(lambda obs: [obs.observation.location.x, obs.observation.location.y, obs.observation.location.z], all_observations))
-        kdtree = KDTree(points)
+        all_points = list(map(lambda obs: [obs.observation.location.x, obs.observation.location.y, obs.observation.location.z], all_observations))
 
         centers = []
+        resulting_observations = []
 
         for observation in all_observations:
+            if observation in observations1.observations:
+                this_sensor_observations = observations1
+                other_sensor_observations = observations2
+            else:
+                this_sensor_observations = observations2
+                other_sensor_observations = observations1
+            current_obs_set = [observation] + other_sensor_observations.observations
+            sensor = this_sensor_observations.header.frame_id
+
+            points = list(map(lambda obs: [obs.observation.location.x, obs.observation.location.y, obs.observation.location.z], current_obs_set))
+            kdtree = KDTree(points)
+
             location = [observation.observation.location.x, observation.observation.location.y, observation.observation.location.z]
             distance, indices = kdtree.query([location], k=2)
             index = indices[0][1]
@@ -171,18 +195,51 @@ class MergeNode:
             index2 = indices2[0][1]
 
             if points[index2] == location and distance[0][1] <= self.merge_distance_threshold:
-                location2 = points[index]
-                center = list((np.asarray(location) + np.asarray(location2))/2)
-                if center not in centers:
-                    centers.append(center)
-                rospy.loginfo(f"\nCenter found at: {center};\n\n")
+                if sensor == self.lidar_sensor_name:
+                    lidar_observation = observation
+                    camera_observation = other_sensor_observations.observations[index-1]
+                else:
+                    camera_observation = observation
+                    lidar_observation = other_sensor_observations.observations[index-1]
+
+                center_observation = self.euclidean_average(lidar_observation, camera_observation)
+                center_location = [center_observation.observation.location.x, center_observation.observation.location.y, center_observation.observation.location.z]
+                if center_location not in centers:
+                    centers.append(center_location)
+                    resulting_observations.append(center_observation)
+
+                rospy.loginfo(f"\nCenter found at: {center_location};\n\n")
+                rospy.loginfo(f"\nLidar at: {lidar_observation.observation.location};\n\n")
             elif location not in centers:
                 centers.append(location)
-
-
-        rospy.loginfo(f"\npoints = {points}")
+                resulting_observations.append(observation)
+        
+        rospy.loginfo(f"\npoints = {all_points}")
         rospy.loginfo(f"\ncolors = {[observation.observation.observation_class for observation in all_observations]}")
         rospy.loginfo(f"\ncenters = {centers}")
+
+        result = ObservationWithCovarianceArrayStamped()
+        result.header.seq = observations1.header.seq
+        result.header.stamp.secs, result.header.stamp.nsecs = observations1.header.stamp.secs, observations1.header.stamp.nsecs
+        result.header.frame_id = observations1.header.frame_id
+        result.observations = resulting_observations
+        self.publish(result)
+
+
+    def euclidean_average(self, lidar_observation, camera_observation):
+        average_observation = ObservationWithCovariance()
+        average_observation.observation.observation_class = lidar_observation.observation.observation_class
+        average_observation.observation.belief = lidar_observation.observation.belief
+        average_observation.covariance = tuple(list(lidar_observation.covariance).copy())
+
+        lidar_observation_location = [lidar_observation.observation.location.x, lidar_observation.observation.location.y, lidar_observation.observation.location.z]
+        camera_observation_location = [camera_observation.observation.location.x, camera_observation.observation.location.y, camera_observation.observation.location.z]
+        average_observation.observation.location.x, average_observation.observation.location.y, average_observation.observation.location.z = list((np.asarray(lidar_observation_location) + np.asarray(camera_observation_location))/2)
+
+        return average_observation
+    
+    def kalman_filter(self, lidar_observation, camera_observation):
+        return
 
 
 node = MergeNode()
