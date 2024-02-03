9130f06d0e8824368895040cdd324a735e544ee2
Robbe1612 <robbegalle1@gmail.com>
2024-02-03 12:02:22 +0100

    duplicate cones bug solved

--- ROS/src/perception/fusion/scripts/early_fusion.py
+++ ROS/src/perception/fusion/scripts/early_fusion.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import threading
 from pathlib import Path
 
 import cv2 as cv
@@ -25,6 +26,8 @@ class EarlyFusion:
         self.pc = None
         self.bbox_header = None
         self.bboxes = None
+        self.new_pc_available = False
+        self.pc_lock = threading.Lock()
         self.cone_width = 0.232
         self.base_link_frame = rospy.get_param("~base_link_frame", "ugr/car_base_link")
         self.world_frame = rospy.get_param("~world_frame", "ugr/map")
@@ -67,11 +70,18 @@ class EarlyFusion:
         self.result_publisher.publish(msg)
 
     def handle_pc(self, msg: PointCloud2):
-        self.pc_header = msg.header
-        rospy.loginfo("Got pointcloud")
-        self.pc = np.array(
-            list(pc2.read_points(msg, skip_nans=True, field_names=("x", "y", "z")))
-        )
+        with self.pc_lock:
+            self.pc_header = msg.header
+            rospy.loginfo("Got pointcloud")
+            if not self.new_pc_available:
+                self.pc = np.array(
+                    list(
+                        pc2.read_points(
+                            msg, skip_nans=True, field_names=("x", "y", "z")
+                        )
+                    )
+                )
+                self.new_pc_available = True
 
     def handle_bbox(self, msg: BoundingBoxesStamped):
         self.bbox_header = msg.header
@@ -81,51 +91,50 @@ class EarlyFusion:
             self.run_fusion()
 
     def run_fusion(self):
-        transformed = self.transform_points(self.pc)
-        results = ObservationWithCovarianceArrayStamped()
-        used_points = []
-        for box in self.bboxes:
-            inside_pts = []
-            distances = []
-            indices = []
-            for i, point in enumerate(transformed):
-                if (
-                    i not in used_points
-                    and point[0][0] > box.left
-                    and point[0][0] < box.left + box.width
-                    and point[0][1] > box.top
-                    and point[0][1] < box.top + box.height
-                ):
-                    inside_pts.append(self.pc[i])
-                    distances.append(np.linalg.norm(self.pc[i]))
-                    indices.append(i)
+        with self.pc_lock:
+            if self.new_pc_available:
+                transformed = self.transform_points(self.pc)
+                results = ObservationWithCovarianceArrayStamped()
+                for box in self.bboxes:
+                    inside_pts = []
+                    indices = []
+                    for i, point in enumerate(transformed):
+                        if (
+                            point[0][0] > box.left
+                            and point[0][0] < box.left + box.width
+                            and point[0][1] > box.top
+                            and point[0][1] < box.top + box.height
+                        ):
+                            inside_pts.append(self.pc[i])
+                            indices.append(i)
 
-            if inside_pts == []:
-                continue
-            closest_distance = min(distances)
-            closest_pt = inside_pts[distances.index(closest_distance)]
-            filtered_pts = []
-            for i, point in enumerate(inside_pts):
-                if np.linalg.norm(point - closest_pt) < 0.5:
-                    filtered_pts.append(point)
-                    used_points.append(indices[i])
-            if not filtered_pts:
-                continue
-            centroid = np.mean(filtered_pts, axis=0)
-            direction_vector = centroid / np.linalg.norm(centroid)
-            centroid += direction_vector * (1 / 6 * self.cone_width)
-            cone = ObservationWithCovariance()
-            cone.observation.location.x = centroid[0]
-            cone.observation.location.y = centroid[1]
-            cone.observation.location.z = centroid[2]
-            cone.covariance = [0.2, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.8]
-            cone.observation.observation_class = box.cone_type
-            cone.observation.belief = 0.8
-            results.observations.append(cone)
-        self.pc = None
-        results.header.stamp = self.bbox_header.stamp
-        results.header.frame_id = self.pc_header.frame_id
-        self.publish(results)
+                    if inside_pts == []:
+                        continue
+                    mean_point = np.median(inside_pts, axis=0)
+                    filtered_pts = []
+                    for point in inside_pts:
+                        if np.linalg.norm(point - mean_point) < 1:
+                            filtered_pts.append(point)
+                    if filtered_pts == []:
+                        continue
+                    centroid = np.mean(filtered_pts, axis=0)
+                    direction_vector = centroid / np.linalg.norm(centroid)
+                    centroid += direction_vector * (1 / 6 * self.cone_width)
+                    cone = ObservationWithCovariance()
+                    cone.observation.location.x = centroid[0]
+                    cone.observation.location.y = centroid[1]
+                    cone.observation.location.z = centroid[2]
+                    cone.covariance = [0.2, 0.0, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0.8]
+                    # cone.covariance = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+                    cone.observation.observation_class = box.cone_type
+                    cone.observation.belief = 0.8
+                    results.observations.append(cone)
+
+                self.pc = None
+                self.new_pc_available = False
+                results.header.stamp = self.bbox_header.stamp
+                results.header.frame_id = self.pc_header.frame_id
+                self.publish(results)
 
     def transform_points(self, points):
         points += self.orig_translation_vector
