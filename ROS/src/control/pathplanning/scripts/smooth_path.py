68c927565b3c7531064eed23927baf4a63aa59da
ZanderDaninck <zander.daninck@gmail.com>
2024-04-11 14:17:17 +0200

    fixed bug

--- ROS/src/control/pathplanning/scripts/smooth_path.py
+++ ROS/src/control/pathplanning/scripts/smooth_path.py
@@ -24,8 +24,7 @@ class PoseArraySmootherNode:
         self.tf_listener = tf.TransformListener(self.tf_buffer)
 
         self.world_frame = rospy.get_param("~world_frame", "ugr/map")
-        self.final_path = False
-        self.path_final = None
+
         # Subscriber and publisher
         self.subscriber = rospy.Subscriber(
             "/input/path", Path, self.pose_array_callback
@@ -33,28 +32,20 @@ class PoseArraySmootherNode:
         self.publisher = rospy.Publisher("/output/path", Path, queue_size=10)
 
     def pose_array_callback(self, msg: Path):
+        """
+        Receives pathplanning path and smooths it using a BSpline
+        """
         try:
-            trans = self.tf_buffer.lookup_transform(
-                self.world_frame, msg.header.frame_id, msg.header.stamp
-            )
+            msg_frame_id = msg.header.frame_id
 
-            transformed_msg = ROSNode.do_transform_path(msg, trans)
-            transformed_msg.header.frame_id = self.world_frame
             mission = rospy.get_param("/mission", "")
-            path = np.array(
-                [[p.pose.position.x, p.pose.position.y] for p in transformed_msg.poses]
-            )
 
-            # Find point closest to 0, 0
-            closest_point = np.argmin(np.sum(path**2, axis=1))
+            path = np.array([[p.pose.position.x, p.pose.position.y] for p in msg.poses])
 
-            # Rotate path so that closest point is first
-            path = np.roll(path, -closest_point, axis=0)
+            # Determine path closure and BSpline periodicity
             away_from_start = False
-
             per = 0  # BSpline periodicity, 0 = not periodic, 1 = periodic
 
-            # Determine loop closure and BSpline periodicity
             for node in path:
                 distance_lc = (path[0, 0] - node[0]) ** 2 + (path[0, 1] - node[1]) ** 2
                 if away_from_start and (
@@ -67,9 +58,34 @@ class PoseArraySmootherNode:
                 ):
                     away_from_start = True
 
+            # Transform closed path to world frame
+            # and shift it away from car
+            # to avoid jumping of path close to car
+            if per and mission == AutonomousMission.TRACKDRIVE:
+                trans = self.tf_buffer.lookup_transform(
+                    self.world_frame, msg.header.frame_id, msg.header.stamp
+                )
+                transformed_msg = ROSNode.do_transform_path(msg, trans)
+
+                msg_frame_id = self.world_frame
+
+                path = np.array(
+                    [
+                        [p.pose.position.x, p.pose.position.y]
+                        for p in transformed_msg.poses
+                    ]
+                )
+
+                # Find point closest to 0, 0
+                closest_point = np.argmin(np.sum(path**2, axis=1))
+
+                # Shift start of path to origin - 10 points so that car never reachs start/stop of path
+                path = np.roll(path, -closest_point - 10, axis=0)
+
             # Add zero pose to path if no closure of path
             if not per and mission == AutonomousMission.TRACKDRIVE:
                 path = np.vstack(([0, 0], path))
+
             # Linear interpolation between center points to add more points for BSpline smoothing
             distance = np.cumsum(np.sqrt(np.sum(np.diff(path, axis=0) ** 2, axis=1)))
             distance = np.insert(distance, 0, 0) / distance[-1]
@@ -79,19 +95,20 @@ class PoseArraySmootherNode:
             path = interpolator(alpha)
 
             # Smooth path with BSpline interpolation
-            path = (
-                path.T
-            )  # Transpose to get correct shape for BSpline, splprep expects (2, N)
-            w = np.array([1] * len(path[0]))  # Weights for BSpline
-            # w[0] = 1
-            # w[-1] = 1
+            # Transpose to get correct shape for BSpline, splprep expects (2, N)
+            path = path.T
 
-            tck, u = splprep(path, w=w, s=10, per=per)  # Calculate BSpline
-            smoothed_path = np.array(
-                splev(u, tck)
-            ).T  # Evaluate BSpline and transpose back to (N, 2)
+            # Weights for BSpline
+            w = np.ones(len(path[0]))
 
-            smoothed_msg = transformed_msg
+            # Calculate smoothing Spline
+            tck, u = splprep(path, w=w, s=2, per=per)
+
+            # Evaluate BSpline and transpose back to (N, 2)
+            smoothed_path = np.array(splev(u, tck)).T
+
+            smoothed_msg = msg
+            smoothed_msg.header.frame_id = msg_frame_id
             smoothed_msg.poses = []
             for point in smoothed_path:
                 pose = PoseStamped()
@@ -99,18 +116,6 @@ class PoseArraySmootherNode:
                 pose.pose.position.y = point[1]
                 smoothed_msg.poses.append(pose)
 
-            if per and not self.final_path:
-                self.final_path = True
-                self.path_final = smoothed_msg
-                print("Final path found")
-                print("Final path found")
-                print("Final path found")
-                print("Final path found")
-                print("Final path found")
-
-            # if self.final_path:
-            #     self.publisher.publish(self.path_final)
-            # else:
             self.publisher.publish(smoothed_msg)
         except Exception as e:
             rospy.logerr("Error occurred while smoothing PoseArray: {}".format(e))
