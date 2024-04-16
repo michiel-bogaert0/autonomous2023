7e998671d97907659096171eb0dea9143f0504db
SiebeVdw <siebe.vandewalle@ugentracing.be>
2024-04-16 19:38:55 +0200

    lint

--- ROS/src/can/scripts/dictionaries.py
+++ ROS/src/can/scripts/dictionaries.py
@@ -1,13 +1,11 @@
-from std_msgs.msg import Float64, Int64, String, Bool
+from std_msgs.msg import Bool, Float64, Int64, String
 
 messages = {
     "<class 'float'>": lambda msg: Float64(msg),
     "<class 'cantools.database.can.signal.NamedSignalValue'>": lambda msg: String(msg),
     "<class 'int'>": lambda msg: Int64(msg),
     "<class 'bool": lambda msg: Bool(msg),
-
 }
-
 
 publishers = {
     # Empty by design. Will be filled with publishers on the fly
