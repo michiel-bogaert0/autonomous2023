f425ff9206bcb43673d9556edcb02cb7592f0499
ZanderDaninck <zander.daninck@gmail.com>
2024-04-16 22:14:37 +0200

    comment

--- ROS/src/control/kinematic_path_tracking/src/kinematic_path_tracking/trajectory.py
+++ ROS/src/control/kinematic_path_tracking/src/kinematic_path_tracking/trajectory.py
@@ -143,6 +143,7 @@ class Trajectory:
 
         # Transfom path to most recent blf
         self.path_blf = self.transform_blf()
+        # Throw away last point to avoid weird FWF bug (see wiki)
         self.path_blf = self.path_blf[:-1]
 
         # No path received
