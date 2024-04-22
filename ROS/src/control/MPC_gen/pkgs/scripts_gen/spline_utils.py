3a91ff3fcebf240e652ab1544f6b1cae6470b57d
ZanderDaninck <zander.daninck@gmail.com>
2024-04-22 11:22:40 +0200

    boundary constraints

--- ROS/src/control/MPC_gen/pkgs/scripts_gen/spline_utils.py
+++ ROS/src/control/MPC_gen/pkgs/scripts_gen/spline_utils.py
@@ -1,3 +1,4 @@
+import casadi as cd
 import matplotlib.pyplot as plt
 import numpy as np
 from optimal_control_gen.Bspline import Bspline
@@ -127,18 +128,104 @@ def get_boundary_constraints(spline, tau, width, plot=False):
     dcurve = spline.derivative(o=1)
     der_point = np.squeeze(dcurve(tau))
 
-    tangent = np.array([der_point[0], der_point[1]])
+    tangent = np.array([-der_point[1], der_point[0]]) / np.linalg.norm(der_point)
 
-    pos_outer = point + width * tangent
-    pos_inner = point - width * tangent
+    # TODO: make width dependent on tau
+    pos_outer = point - width * tangent
+    pos_inner = point + width * tangent
 
     track_constraint_lower = np.sum(tangent * pos_inner)
     track_constraint_upper = np.sum(tangent * pos_outer)
 
+    print(np.sum(tangent * point))
+
     print(f"Track constraint lower: {track_constraint_lower}")
     print(f"Track constraint upper: {track_constraint_upper}")
+
+    # get line through pos_inner
+    slope_inner = -tangent[0] / tangent[1]
+    intercept_inner = pos_inner[1] - slope_inner * pos_inner[0]
+
+    # get line through pos_outer
+    slope_outer = -tangent[0] / tangent[1]
+    intercept_outer = pos_outer[1] - slope_outer * pos_outer[0]
+    point_test = np.copy(point)
+    point_test[0] -= 1.5
+    point_test[1] += 1.5
+    in_between = (slope_inner * point_test[0] + intercept_inner - point_test[1]) * (
+        slope_outer * point_test[0] + intercept_outer - point_test[1]
+    ) < 0
+    print(in_between)
 
     if plot:
         plt.plot(point[0], point[1], "o", c="r")
         plt.plot(pos_inner[0], pos_inner[1], "o", c="b")
         plt.plot(pos_outer[0], pos_outer[1], "o", c="y")
+
+        # generate x around point
+        x = np.linspace(point[0] - 2, point[0] + 2, 100)
+        y_inner = slope_inner * x + intercept_inner
+        y_outer = slope_outer * x + intercept_outer
+        plt.plot(x, y_inner, c="b")
+        plt.plot(x, y_outer, c="y")
+
+        plt.plot(point_test[0], point_test[1], "o", c="g")
+
+    return slope_inner, intercept_inner, slope_outer, intercept_outer
+
+
+def get_boundary_constraints_casadi(spline, tau, width, plot=False):
+    """
+    Get the boundary constraints for the spline
+    """
+    point = spline(tau).T
+
+    # Get the derivative of the spline
+    dcurve = spline.derivative(o=1)
+    der_point = dcurve(tau).T
+
+    norm = cd.norm_2(der_point)
+    # norm = cd.if_else(norm < 1e-1, 1, norm)
+    # norm = cd.fmax(1e-1, norm)
+    tangent = cd.vertcat(-der_point[1], der_point[0]) / norm
+
+    # TODO: make width dependent on tau
+    pos_outer = point - width * tangent
+    pos_inner = point + width * tangent
+
+    # track_constraint_lower = cd.sum2(tangent * pos_inner)
+    # track_constraint_upper = cd.sum2(tangent * pos_outer)
+
+    # print(np.sum(tangent * point))
+
+    # print(f"Track constraint lower: {track_constraint_lower}")
+    # print(f"Track constraint upper: {track_constraint_upper}")
+
+    # get line through pos_inner
+    # slope_inner = cd.if_else(tangent[1] == 0, 0, -tangent[0] / tangent[1])
+    slope_inner = 1
+    intercept_inner = pos_inner[1] - slope_inner * pos_inner[0]
+
+    # get line through pos_outer
+    slope_outer = slope_inner
+    intercept_outer = pos_outer[1] - slope_outer * pos_outer[0]
+    # point_test = np.copy(point)
+    # point_test[0] -= 3.5
+    # in_between = (slope_inner * point_test[0] + intercept_inner - point_test[1]) * (
+    #     slope_outer * point_test[0] + intercept_outer - point_test[1]
+    # ) < 0
+    # print(in_between)
+
+    if plot:
+        plt.plot(point[0], point[1], "o", c="r")
+        plt.plot(pos_inner[0], pos_inner[1], "o", c="b")
+        plt.plot(pos_outer[0], pos_outer[1], "o", c="y")
+
+        # generate x around point
+        x = np.linspace(point[0] - 2, point[0] + 2, 100)
+        y_inner = slope_inner * x + intercept_inner
+        y_outer = slope_outer * x + intercept_outer
+        plt.plot(x, y_inner, c="b")
+        plt.plot(x, y_outer, c="y")
+
+    return slope_inner, intercept_inner, slope_outer, intercept_outer
