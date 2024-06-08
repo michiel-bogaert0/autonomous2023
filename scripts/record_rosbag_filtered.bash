4532366860d9f9d3002615b7f8867488e4cc24ad
ZanderDaninck <zander.daninck@gmail.com>
2024-06-08 12:32:41 +0200

    don't delete by default

--- scripts/record_rosbag_filtered.bash
+++ scripts/record_rosbag_filtered.bash
@@ -10,4 +10,8 @@ filtered_bag_name=$current_date_time"_"$reason.bag
 # Filter bag
 rosbag filter $current_date_time.bag $filtered_bag_name "topic not in ['/ugr/car/sensors/cam0/image']"
 
-rm $current_date_time.bag
+read -p "Do you want to delete the original rosbag? (y/n): " delete_original
+
+if [ $delete_original == "y" ]; then
+    rm $current_date_time.bag
+fi
