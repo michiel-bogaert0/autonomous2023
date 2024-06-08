d04a43ba812a373d5a37a297e9e71538a64d3818
ZanderDaninck <zander.daninck@gmail.com>
2024-06-08 12:41:32 +0200

    better script

--- scripts/record_rosbag_filtered.bash
+++ scripts/record_rosbag_filtered.bash
@@ -3,12 +3,23 @@
 current_date_time=$(date +"%Y-%m-%d-%H-%M-%S")
 rosbag record -a -O $current_date_time
 
-read -p "Specify reason for rosbag recording: " reason
+read -p "Give rosbag description (enter if no description): " reason
 
-filtered_bag_name=$current_date_time"_"$reason.bag
+if [ -z "$reason" ]; then
+    filtered_bag_name=$current_date_time
+else
+filtered_bag_name=$current_date_time"_"$reason
+fi
+
+read -p "Filter rosbag? (y/n): " filter
+
+if [ $filter == "n" ]; then
+    mv $current_date_time.bag $filtered_bag_name.bag
+    exit 0
+fi
 
 # Filter bag
-rosbag filter $current_date_time.bag $filtered_bag_name "topic not in ['/ugr/car/sensors/cam0/image']"
+rosbag filter $current_date_time.bag $filtered_bag_name"_filtered.bag" "topic not in ['/ugr/car/sensors/cam0/image']"
 
 read -p "Do you want to delete the original rosbag? (y/n): " delete_original
 
