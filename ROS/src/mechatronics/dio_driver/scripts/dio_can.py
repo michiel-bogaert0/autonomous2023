df388755661abb2f155bdb59eec0d0ff6a3eb96a
Lowiek <Lowiek@fablabfactory.com>
2024-06-24 00:30:35 +0200

    Cleaned up DIO node

--- ROS/src/mechatronics/dio_driver/scripts/dio_can.py
+++ ROS/src/mechatronics/dio_driver/scripts/dio_can.py
@@ -9,8 +9,16 @@ from std_msgs.msg import Bool
 
 class DioCAN(ManagedNode):
     def __init__(self) -> None:
-        super().__init__("pure_pursuit_control", "active")
+        """This node is responsible for handling the digital inputs and outputs of the car.
 
+        - Sends the the DI at the same rate of the CAN messages.
+        - Sends the DO at a fixed rate (parameter ~rate) and immediately when a change is detected.
+
+        Note that this node DOES NOT send the ECU heartbeat of the DIO Bank, as required by the module.
+        """
+        super().__init__("diobank_driver_can")
+
+        # Publishers and subscribers. 10 DI and 8 DO
         self.DI_publishers = [
             rospy.Publisher(f"/di/{i}", Bool, queue_size=10) for i in range(10)
         ]
@@ -24,7 +32,8 @@ class DioCAN(ManagedNode):
 
         self.DO_state = 0x0
 
-    def handle_DO_change(self, dig_out_id, msg):
+    def handle_DO_change(self, dig_out_id, msg: Bool):
+        """When a digital output changes, this function is called to update the state of the digital outputs."""
         if msg.data:
             self.DO_state = self.DO_state | (0x80 >> dig_out_id)
         else:
@@ -33,6 +42,9 @@ class DioCAN(ManagedNode):
         self.send_over_can()
 
     def send_over_can(self):
+        """
+        Sends the current state of the digital outputs over CAN.
+        """
         msg = Frame()
         msg.id = 258
         msg.is_extended = False
@@ -49,6 +61,10 @@ class DioCAN(ManagedNode):
         # First 10 bits: DI
         # Following 8 bits: DO
         # Publish on publishers
+
+        if msg.id != 0x101:
+            rospy.logerr(f"Received message with ID {msg.id}, expected 0x101")
+            return
 
         value = msg.data[0] << 8 + (msg.data[1] & 0xC0)
 
