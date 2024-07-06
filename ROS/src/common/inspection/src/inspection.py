dd1cb1ef33763eb2fde7f5619216c70d8a1f70fb
Lowie Slachmuylders <slachmuylders.lowie@gmail.com>
2024-07-06 12:39:05 +0200

    start and brake

--- ROS/src/common/inspection/src/inspection.py
+++ ROS/src/common/inspection/src/inspection.py
@@ -2,8 +2,10 @@
 import math
 
 import rospy
+from node_fixture import AutonomousStatesEnum
 from node_fixture.node_manager import NodeManager
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, UInt16
+from ugr_msgs.msg import State
 
 
 class Inspection(NodeManager):
@@ -23,23 +25,47 @@ class Inspection(NodeManager):
         rospy.loginfo("Configuring inspection node")
         self.STEERING_SPEED = rospy.get_param("~steering_speed", 0.05)
         self.VELOCITY = rospy.get_param("~velocity", 3.5)
+        self.DURATION = rospy.get_param("~duration", 25)
         self.counter = 0
 
         self.steering_pub = rospy.Publisher("/output/steering", Float64)
         self.velocity_pub = rospy.Publisher("/output/velocity", Float64)
+        self.lap_complete_pub = rospy.Publisher(
+            "/output/lapComplete", UInt16, queue_size=5, latch=True
+        )
+
+        self.state_as_sub = rospy.Subscriber(
+            "/state/as", State, self.handle_state_change
+        )
+
+        self.start = False
+
+    def handle_state_change(self, msg: State):
+        if msg.cur_state == AutonomousStatesEnum.ASDRIVE:
+            rospy.loginfo("Starting inspection")
+            self.stop_time = rospy.Time.now() + rospy.Duration(self.DURATION)
+            self.start = True
 
     def active(self):
         """
         Inspection node is active
         """
-        steering_angle = math.pi / 2 * math.sin(self.counter * self.STEERING_SPEED)
+        if not self.start:
+            return
 
-        self.steering_pub.publish(steering_angle)
-        self.velocity_pub.publish(self.VELOCITY)
+        if rospy.Time.now() > self.stop_time:
+            self.velocity_pub.publish(0)
+            self.lap_complete_pub.publish(1)
+        else:
+            steering_angle = math.pi / 2 * math.sin(self.counter * self.STEERING_SPEED)
 
-        self.counter += 1
+            self.steering_pub.publish(steering_angle)
+            self.velocity_pub.publish(self.VELOCITY)
 
-        rospy.loginfo(f"Steering angle: {steering_angle}, velocity: {self.VELOCITY}")
+            self.counter += 1
+            rospy.loginfo(
+                f"Steering angle: {steering_angle}, velocity: {self.VELOCITY}"
+            )
 
 
 node = Inspection()
