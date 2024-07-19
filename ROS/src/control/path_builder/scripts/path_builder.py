fb61ed8c898b3764bed349368c4a767740e0e039
ZanderDaninck <zander.daninck@gmail.com>
2024-07-19 22:01:49 +0200

    implemented path builder

--- ROS/src/control/path_builder/scripts/path_builder.py
+++ ROS/src/control/path_builder/scripts/path_builder.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import rospy
+import tf2_ros as tf
+from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 from node_fixture.managed_node import ManagedNode
-from ugr_msgs.msg import PathIds
+from std_msgs.msg import UInt16
+from ugr_msgs.msg import ObservationWithCovarianceArrayStamped, PathIds
 
 
 class PathBuilder(ManagedNode):
@@ -12,26 +16,114 @@ class PathBuilder(ManagedNode):
         self.spin()
 
     def doConfigure(self):
+        self.world_frame = rospy.get_param("~world_frame", "ugr/map")
+        self.base_link_frame = rospy.get_param("~base_link_frame", "ugr/car_base_link")
         self.publisher = super().AddPublisher("/output/path", Path, queue_size=10)
+        self.lap_complete_sub = super().AddSubscriber(
+            "/ugr/car/lapComplete", UInt16, self.lap_complete_callback
+        )
+        self.closed = False
+        self.global_path_ids = []
+
+        self.cones = None
+
+        self.tf_buffer = tf.Buffer()
+        self.tf_listener = tf.TransformListener(self.tf_buffer)
 
     def doActivate(self):
+        self.map_sub = super().AddSubscriber(
+            "/input/local_map",
+            ObservationWithCovarianceArrayStamped,
+            self.receive_new_map,
+        )
         self.subscriber = super().AddSubscriber(
             "/input/path", PathIds, self.path_callback
         )
+
+    def lap_complete_callback(self, msg: UInt16):
+        if msg.data >= 1:
+            self.closed = True
+
+    def receive_new_map(self, map):
+        if map is None:
+            return
+        self.cones = np.zeros((len(map.observations), 4))
+        merges = {}
+        for i, obs in enumerate(map.observations):
+            self.cones[i] = [
+                obs.observation.location.x,
+                obs.observation.location.y,
+                obs.observation.observation_class,
+                obs.observation.id,
+            ]
+            merges[obs.observation.id] = obs.observation.merged_ids
 
     def path_callback(self, msg: PathIds):
         """
         Creates global path, given pathplanning path and IDs
         """
-        rospy.loginfo("Received path")
-        # rospy.loginfo(msg)
-        path = Path()
-        path.header = msg.header
-        for pose in msg.poses:
-            print(pose)
-            path.poses.append(pose)
+        # Store IDs of first pose of each pathplanning path
+        closest_point_ids = (msg.poses[0].left_id, msg.poses[0].right_id)
 
-        self.publisher.publish(path)
+        print(closest_point_ids)
+
+        if closest_point_ids not in self.global_path_ids:
+            self.global_path_ids.append(closest_point_ids)
+
+        if not self.closed:
+            # Just publish the pathplanning path
+            path = Path()
+            path.header = msg.header
+            for pose in msg.poses:
+                path.poses.append(pose)
+
+            self.publisher.publish(path)
+        else:
+            rospy.loginfo("Closed loop detected")
+            rospy.loginfo("Closed loop detected")
+            rospy.loginfo("Closed loop detected")
+            rospy.loginfo("Closed loop detected")
+            rospy.loginfo("Closed loop detected")
+            rospy.loginfo("Global Path Ids: {}".format(self.global_path_ids))
+            global_path = []
+            for tuple in self.global_path_ids:
+                left_cone = None
+                right_cone = None
+                for cone in self.cones:
+                    if cone[3] == tuple[0]:
+                        left_cone = cone
+                    if cone[3] == tuple[1]:
+                        right_cone = cone
+                if left_cone is not None and right_cone is not None:
+                    new_point = (
+                        (left_cone[0] + right_cone[0]) / 2,
+                        (left_cone[1] + right_cone[1]) / 2,
+                    )
+                    if len(global_path) >= 2:
+                        dist_new_to_second_last_point = (
+                            (new_point[0] - global_path[-2][0]) ** 2
+                            + (new_point[1] - global_path[-2][1]) ** 2
+                        ) ** 0.5
+                        dist_last_to_second_last_point = (
+                            (global_path[-1][0] - global_path[-2][0]) ** 2
+                            + (global_path[-1][1] - global_path[-2][1]) ** 2
+                        ) ** 0.5
+
+                        if (
+                            dist_new_to_second_last_point
+                            > dist_last_to_second_last_point
+                        ):
+                            global_path.append(new_point)
+                    else:
+                        global_path.append(new_point)
+            path = Path()
+            path.header = msg.header
+            for point in global_path:
+                pose = PoseStamped()
+                pose.pose.position.x = point[0]
+                pose.pose.position.y = point[1]
+                path.poses.append(pose)
+            self.publisher.publish(path)
 
 
 PathBuilder()
