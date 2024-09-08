29edc72a5016d7d53ef730fcc57e28fc6e5028e1
michiel-bogaert0 <141361768+michiel-bogaert0@users.noreply.github.com>
2024-09-08 22:10:47 +0200

    test - exclude hpp files from cppcheck

--- ROS/src/perception/lidar/include/ground_removal.hpp
+++ ROS/src/perception/lidar/include/ground_removal.hpp
@@ -42,50 +42,33 @@ private:
   std::string ground_removal_method_; // Default: bins, others: zermas
 
   double min_distance_; // Preprocessing parameters
+  double max_distance_; //
+  double min_angle_;    //
+  double max_angle_;    //
 
-  double max_distance_;
-
-  double min_angle_;
-
-  double max_angle_;
-
-  int num_iter_; // Number of iterations
-
-  int num_lpr_; // number of points used to estimate the LPR: Lowest Point
-                // Representative -> a point defined as the average of the
-                // num_lpr_ lowest points
-
+  int num_iter_;    // Number of iterations
+  int num_lpr_;     // number of points used to estimate the LPR: Lowest Point
+                    // Representative -> a point defined as the average of the
+                    // num_lpr_ lowest points
   double th_seeds_; // Threshold for points to be considered initial seeds
-
-  double th_dist_; // Threshold distance from the plane
-
+  double th_dist_;  // Threshold distance from the plane
   double sensor_height_; // GroundRemoval sensor height to ground (m)
-
-  double th_floor_; // Threshold distance from the floor level
-
-  int angular_buckets_; // number of angular levels for buckets
-
+  double th_floor_;      // Threshold distance from the floor level
+  int angular_buckets_;  // number of angular levels for buckets
   double radial_bucket_tipping_point_; // edge between small buckets and big
                                        // buckets
+  double small_radial_bucket_length_;  // length of small buckets
+  double big_radial_bucket_length_;    // length of big buckets
+  bool use_slope_;                     // Is the slope used for GR or not
 
-  double small_radial_bucket_length_; // length of small buckets
-
-  double big_radial_bucket_length_; // length of big buckets
-
-  bool use_slope_; // Is the slope used for GR or not
-
-  bool noisy_environment_; // whether the lidar is used in a busy/noisy room
-                           // (more points)
-
-  double max_bucket_height_; // maximum height difference between top points
-                             // and lower points in bin for noisy environments
-
+  bool noisy_environment_;    // whether the lidar is used in a busy/noisy room
+                              // (more points)
+  double max_bucket_height_;  // maximum height difference between top points
+                              // and lower points in bin for noisy environments
   int min_points_per_bucket_; // minimum points allowed in a bucket for noisy
                               // environments
-
   int max_points_per_bucket_; // maximum points allowed in a bucket for noisy
                               // environments
-
   int factor_color_; // number of consecutive points that get the same color
 
   void groundRemovalZermas(
@@ -118,9 +101,7 @@ private:
     bool operator()(pcl::PointXYZINormal a, pcl::PointXYZINormal b) const {
       return a.y > b.y;
     }
-  }
-
-  leftrightsort;
+  } leftrightsort;
 };
 } // namespace ns_lidar
 
